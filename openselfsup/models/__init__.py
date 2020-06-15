b7a1eb46a21c89641e30b0727908e3e06c0b1f86
xiaohangzhan <xiaohangzhan@outlook.com>
2020-06-16 01:44:25 +0800

    remove unrelated code

--- openselfsup/models/__init__.py
+++ openselfsup/models/__init__.py
@@ -4,7 +4,6 @@ from .heads import *
 from .classification import Classification
 from .deepcluster import DeepCluster
 from .odc import ODC
-from .losses import *  # noqa: F401,F403
 from .necks import *
 from .npid import NPID
 from .memories import *
@@ -12,9 +11,3 @@ from .moco import MOCO
 from .registry import (BACKBONES, MODELS, NECKS, MEMORIES, HEADS, LOSSES)
 from .rotation_pred import RotationPred
 from .simclr import SimCLR
-
-#__all__ = [
-#    'BACKBONES', 'NECKS', 'ROI_EXTRACTORS', 'SHARED_HEADS', 'HEADS', 'LOSSES',
-#    'DETECTORS', 'CLASSIFIERS', 'build_backbone', 'build_neck', 'build_roi_extractor',
-#    'build_shared_head', 'build_head', 'build_loss', 'build_detector', 'build_detector'
-#]
