c94991abe8766988410bbdd19543adb07fd7a78d
Xiaohang Zhan <xiaohangzhan@outlook.com>
2020-07-19 18:05:14 +0800

    8GPUs evaluation for Places

--- configs/benchmarks/linear_classification/places205/r50_multihead_sobel.py
+++ configs/benchmarks/linear_classification/places205/r50_multihead_sobel.py
@@ -50,7 +50,7 @@ test_pipeline = [
     dict(type='Normalize', **img_norm_cfg),
 ]
 data = dict(
-    imgs_per_gpu=256,  # total 256
+    imgs_per_gpu=32,  # total 32x8=256
     workers_per_gpu=5,
     train=dict(
         type=dataset_type,
