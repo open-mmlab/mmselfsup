0d185ce2cd29e14d24ecb565f3983b275b70aceb
hezijian <hezijian@dm-ai.cn>
2020-10-21 14:07:09 +0800

    add byol with sync-bn and accmulate gradient

--- openselfsup/hooks/byol_hook.py
+++ openselfsup/hooks/byol_hook.py
@@ -1,5 +1,6 @@
 from math import cos, pi
 from mmcv.runner import Hook
+from mmcv.parallel import is_module_wrapper
 
 from .registry import HOOKS
 
@@ -17,17 +18,26 @@ class BYOLHook(Hook):
             for the target network. Default: 1.
     """
 
-    def __init__(self, end_momentum=1., **kwargs):
+    def __init__(self, end_momentum=1., update_interval=1, **kwargs):
         self.end_momentum = end_momentum
+        self.update_interval = update_interval
 
     def before_train_iter(self, runner):
         assert hasattr(runner.model.module, 'momentum'), \
             "The runner must have attribute \"momentum\" in BYOLHook."
         assert hasattr(runner.model.module, 'base_momentum'), \
             "The runner must have attribute \"base_momentum\" in BYOLHook."
-        cur_iter = runner.iter
-        max_iter = runner.max_iters
-        base_m = runner.model.module.base_momentum
-        m = self.end_momentum - (self.end_momentum - base_m) * (
-            cos(pi * cur_iter / float(max_iter)) + 1) / 2
-        runner.model.module.momentum = m
+        if self.every_n_iters(runner, self.update_interval):
+            cur_iter = runner.iter
+            max_iter = runner.max_iters
+            base_m = runner.model.module.base_momentum
+            m = self.end_momentum - (self.end_momentum - base_m) * (
+                cos(pi * cur_iter / float(max_iter)) + 1) / 2
+            runner.model.module.momentum = m
+
+    def after_train_iter(self, runner):
+        if self.every_n_iters(runner, self.update_interval):
+            if is_module_wrapper(runner.model):
+                runner.model.module.momentum_update()
+            else:
+                runner.model.momentum_update()
