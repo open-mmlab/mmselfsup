34a5455f60d4d22122d24da9e289fb7358a0b340
xieenze <Johnny_ez@163.com>
2020-12-25 10:32:24 +0800

    add byol r50_bs2048_accumulate2_ep200_fp16.py

--- configs/selfsup/byol/r50_bs2048_accumulate2_ep200_fp16.py
+++ configs/selfsup/byol/r50_bs2048_accumulate2_ep200_fp16.py
@@ -4,24 +4,30 @@ _base_ = '../../base.py'
 model = dict(
     type='BYOL',
     pretrained=None,
-    base_momentum=0.996,
+    base_momentum=0.99,
+    pre_conv=True,
     backbone=dict(
         type='ResNet',
         depth=50,
         in_channels=3,
         out_indices=[4],  # 0: conv-1, x: stage-x
-        norm_cfg=dict(type='BN')),
+        norm_cfg=dict(type='SyncBN')),
     neck=dict(
-        type='NonLinearNeckV2',
+        type='NonLinearNeckSimCLR',
         in_channels=2048,
         hid_channels=4096,
         out_channels=256,
+        num_layers=2,
+        sync_bn=True,
+        with_bias=True,
+        with_last_bn=False,
         with_avg_pool=True),
     head=dict(type='LatentPredictHead',
               size_average=True,
-              predictor=dict(type='NonLinearNeckV2',
+              predictor=dict(type='NonLinearNeckSimCLR',
                              in_channels=256, hid_channels=4096,
-                             out_channels=256, with_avg_pool=False)))
+                             out_channels=256, num_layers=2, sync_bn=True,
+                             with_bias=True, with_last_bn=False, with_avg_pool=False)))
 # dataset settings
 data_source_cfg = dict(
     type='ImageNet',
@@ -32,7 +38,7 @@ data_train_root = 'data/imagenet/train'
 dataset_type = 'BYOLDataset'
 img_norm_cfg = dict(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 train_pipeline = [
-    dict(type='RandomResizedCrop', size=224, interpolation=3), # bicubic
+    dict(type='RandomResizedCrop', size=224, interpolation=3),
     dict(type='RandomHorizontalFlip'),
     dict(
         type='RandomAppliedTrans',
@@ -68,7 +74,7 @@ train_pipeline2[4]['p'] = 0.1 # gaussian blur
 train_pipeline2[5]['p'] = 0.2 # solarization
 
 data = dict(
-    imgs_per_gpu=128,  # total 128*32=4096(32gpus)
+    imgs_per_gpu=256,  # total 256*8(gpu)*2(interval)=4096
     workers_per_gpu=4,
     train=dict(
         type=dataset_type,
@@ -80,14 +86,19 @@ data = dict(
         prefetch=prefetch,
     ))
 # additional hooks
+update_interval = 2  # interval for accumulate gradient
 custom_hooks = [
-    dict(type='BYOLHook', end_momentum=1.)
+    dict(type='BYOLHook', end_momentum=1., update_interval=update_interval)
 ]
 # optimizer
-optimizer = dict(type='LARS', lr=3.2, weight_decay=0.0000015, momentum=0.9,
+optimizer = dict(type='LARS', lr=4.8, weight_decay=0.000001, momentum=0.9,
                  paramwise_options={
                     '(bn|gn)(\d+)?.(weight|bias)': dict(weight_decay=0., lars_exclude=True),
                     'bias': dict(weight_decay=0., lars_exclude=True)})
+# apex
+use_fp16 = True
+optimizer_config = dict(update_interval=update_interval, use_fp16=use_fp16)
+
 # learning policy
 lr_config = dict(
     policy='CosineAnnealing',
