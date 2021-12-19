9fa79de299704e41fea071a7bd5463cbe6ec5f5a
liuyuan <liuyuan@pjlab.org.cn>
2021-12-19 20:32:02 +0800

    [Feature]: Add run

---
+++ run/mae_b256_16gpus.py
@@ -0,0 +1,7 @@
+config='configs/selfsup/mae/mae_vit-base_b256-coslr-1600e_in1k.py'
+work_dirs='work_dirs/selfsup/mae_imagenet_pretrain_b256_16gpus'
+job_name='mae_256_16'
+srun_args='--quotatype=spot'
+partition='mm_dev'
+gpus_per_node=8
+gpus=16
