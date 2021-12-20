cddc03e2d9f7fe353d1b4fb958e5ad9d4d7a1070
liuyuan1.vendor <liuyuan@pjlab.org.cn>
2021-12-20 20:13:24 +0800

    [Fix]: Add 1424 specific settings

--- run/mae_b4096_100e.py
+++ run/mae_b4096_100e.py
@@ -1,7 +1,7 @@
 config = 'configs/selfsup/mae/mae_vit-base_b4096-coslr-100e_in1k.py'
 work_dirs = 'work_dirs/selfsup/mae_imagenet_pretrain_b4096_100e'
 job_name = 'mae_4096_100'
-srun_args = ''
-partition = 'mediaf'
+srun_args = '--quotatype=spot --async'
+partition = 'mm_model'
 gpus_per_node = 8
 gpus = 32
