2526130420cbbcc72869c3ac75189f6346e9c709
liuyuan <liuyuan@pjlab.org.cn>
2021-12-20 14:30:37 +0800

    [Refactor]: Refactor mae config file

--- run/generate_cmd.py
+++ run/generate_cmd.py
@@ -1,19 +1,30 @@
-from mae_b128_32gpus import *
+from mae_b4096_16gpus_400e import *
 
-nohup=True
-output_file='test_6.txt'
-resume_from="/mnt/lustre/liuyuan1.vendor/ckptp/mae/mae_imagenet_pretrain_b128_32gpus/epoch_12.pth"
+nohup = True
+output_file = 'test_9.txt'
+resume_from = None
 
 if nohup:
-    cmd = f"GPUS_PER_NODE={gpus_per_node} GPUS={gpus} SRUN_ARGS={f'{srun_args}'} \
-        nohup bash tools/slurm_train.sh {partition} \
-        {job_name} {config} {work_dirs} --resume_from {resume_from} \
-        > f{output_file} &"
+    if resume_from is not None:
+        cmd = f"GPUS_PER_NODE={gpus_per_node} GPUS={gpus} SRUN_ARGS={f'{srun_args}'} \
+            nohup bash tools/slurm_train.sh {partition} \
+            {job_name} {config} {work_dirs} --resume_from {resume_from} \
+            > f{output_file} &"
+
+    else:
+        cmd = f"GPUS_PER_NODE={gpus_per_node} GPUS={gpus} SRUN_ARGS={f'{srun_args}'} \
+            nohup bash tools/slurm_train.sh {partition} \
+            {job_name} {config} {work_dirs} > f{output_file} &"
 
 else:
-    cmd = f"GPUS_PER_NODE={gpus_per_node} GPUS={gpus} SRUN_ARGS={f'{srun_args}'} \
-        bash tools/slurm_train.sh {partition} \
-        {job_name} {config} {work_dirs} --resume_from {resume_from}"
+    if resume_from is not None:
+        cmd = f"GPUS_PER_NODE={gpus_per_node} GPUS={gpus} SRUN_ARGS={f'{srun_args}'} \
+            bash tools/slurm_train.sh {partition} \
+            {job_name} {config} {work_dirs} --resume_from {resume_from}"
 
+    else:
+        cmd = f"GPUS_PER_NODE={gpus_per_node} GPUS={gpus} SRUN_ARGS={f'{srun_args}'} \
+            bash tools/slurm_train.sh {partition} \
+            {job_name} {config} {work_dirs}"
 
 print(cmd)
