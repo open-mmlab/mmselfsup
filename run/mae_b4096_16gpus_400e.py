0606f919dc366207fedc2fa9eb68165ac6c54f91
liuyuan <liuyuan@pjlab.org.cn>
2021-12-20 14:30:37 +0800

    [Refactor]: Refactor mae config file

---
+++ run/mae_b4096_16gpus_400e.py
@@ -0,0 +1,7 @@
+config = 'configs/selfsup/mae/mae_vit-base_b4096-coslr-400e_in1k.py'
+work_dirs = 'work_dirs/selfsup/mae_imagenet_pretrain_b4096_400e_16gpus'
+job_name = 'mae_4096_400'
+srun_args = '--quotatype=spot'
+partition = 'mm_dev'
+gpus_per_node = 8
+gpus = 16
