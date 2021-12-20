90c5a9526c2000d8620c5588238e75f8c127e491
liuyuan1.vendor <liuyuan@pjlab.org.cn>
2021-12-20 19:34:43 +0800

    [Refactor]: Delete gpus in script

--- run/mae_b4096_100e.py
+++ run/mae_b4096_100e.py
@@ -1,5 +1,5 @@
 config = 'configs/selfsup/mae/mae_vit-base_b4096-coslr-100e_in1k.py'
-work_dirs = 'work_dirs/selfsup/mae_imagenet_pretrain_b4096_100e_16gpus'
+work_dirs = 'work_dirs/selfsup/mae_imagenet_pretrain_b4096_100e'
 job_name = 'mae_4096_100'
 srun_args = ''
 partition = 'mediaf'
