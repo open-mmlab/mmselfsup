bcf24dc261ed9046557a7b15fd7d6424da23da3e
liuyuan1.vendor <liuyuan@pjlab.org.cn>
2021-12-20 20:12:27 +0800

    [Fix]: Make generate_cmd to add --async

--- run/generate_cmd.py
+++ run/generate_cmd.py
@@ -6,25 +6,25 @@ resume_from = None
 
 if nohup:
     if resume_from is not None:
-        cmd = f"GPUS_PER_NODE={gpus_per_node} GPUS={gpus} SRUN_ARGS={f'{srun_args}'} \
+        cmd = f'GPUS_PER_NODE={gpus_per_node} GPUS={gpus} SRUN_ARGS="{srun_args}" \
             nohup bash tools/slurm_train.sh {partition} \
             {job_name} {config} {work_dirs} --resume_from {resume_from} \
-            > f{output_file} &"
+            > f{output_file} &'
 
     else:
-        cmd = f"GPUS_PER_NODE={gpus_per_node} GPUS={gpus} SRUN_ARGS={f'{srun_args}'} \
+        cmd = f'GPUS_PER_NODE={gpus_per_node} GPUS={gpus} SRUN_ARGS="{srun_args}" \
             nohup bash tools/slurm_train.sh {partition} \
-            {job_name} {config} {work_dirs} > f{output_file} &"
+            {job_name} {config} {work_dirs} > {output_file} &'
 
 else:
     if resume_from is not None:
-        cmd = f"GPUS_PER_NODE={gpus_per_node} GPUS={gpus} SRUN_ARGS={f'{srun_args}'} \
+        cmd = f'GPUS_PER_NODE={gpus_per_node} GPUS={gpus} SRUN_ARGS="{srun_args}" \
             bash tools/slurm_train.sh {partition} \
-            {job_name} {config} {work_dirs} --resume_from {resume_from}"
+            {job_name} {config} {work_dirs} --resume_from {resume_from}'
 
     else:
-        cmd = f"GPUS_PER_NODE={gpus_per_node} GPUS={gpus} SRUN_ARGS={f'{srun_args}'} \
+        cmd = f'GPUS_PER_NODE={gpus_per_node} GPUS={gpus} SRUN_ARGS="{srun_args}" \
             bash tools/slurm_train.sh {partition} \
-            {job_name} {config} {work_dirs}"
+            {job_name} {config} {work_dirs}'
 
 print(cmd)
