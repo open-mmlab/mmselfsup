d19b916dd9107d8dcab2ef1d9ed4cdc793b6399e
liuyuan <liuyuan@pjlab.org.cn>
2021-12-20 10:24:55 +0800

    [Feature]: Add auto script to generate run cmd

--- run/generate_cmd.py
+++ run/generate_cmd.py
@@ -1,7 +1,19 @@
-from mae_b256_16gpus import *
+from mae_b128_32gpus import *
 
-cmd = f"GPUS_PER_NODE={gpus_per_node} GPUS={gpus} SRUN_ARGS={f'{srun_args}'} \
-    bash tools/slurm_train.sh {partition} \
-    {job_name} {config} {work_dirs}"
+nohup=True
+output_file='test_6.txt'
+resume_from="/mnt/lustre/liuyuan1.vendor/ckptp/mae/mae_imagenet_pretrain_b128_32gpus/epoch_12.pth"
+
+if nohup:
+    cmd = f"GPUS_PER_NODE={gpus_per_node} GPUS={gpus} SRUN_ARGS={f'{srun_args}'} \
+        nohup bash tools/slurm_train.sh {partition} \
+        {job_name} {config} {work_dirs} --resume_from {resume_from} \
+        > f{output_file} &"
+
+else:
+    cmd = f"GPUS_PER_NODE={gpus_per_node} GPUS={gpus} SRUN_ARGS={f'{srun_args}'} \
+        bash tools/slurm_train.sh {partition} \
+        {job_name} {config} {work_dirs} --resume_from {resume_from}"
+
 
 print(cmd)
