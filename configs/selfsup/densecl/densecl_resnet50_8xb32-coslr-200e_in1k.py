3237c76474aa0d1b7e1cf1c1d8f97c49060c27a9
Yixiao Fang <36138628+fangyixiao18@users.noreply.github.com>
2021-12-23 21:50:31 +0800

    [Docs] add logs for part of algorithms (#157)
    * [Docs] add logs for part of algorithms
    
    * [Fix] fix typo

--- configs/selfsup/densecl/densecl_resnet50_8xb32-coslr-200e_in1k.py
+++ configs/selfsup/densecl/densecl_resnet50_8xb32-coslr-200e_in1k.py
@@ -1,5 +1,5 @@
 _base_ = [
-    '../_base_/models/denscl.py'
+    '../_base_/models/densecl.py',
     '../_base_/datasets/imagenet_mocov2.py',
     '../_base_/schedules/sgd_coslr-200e_in1k.py',
     '../_base_/default_runtime.py',
