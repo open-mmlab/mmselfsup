d992b9cce243fd9801dc5a945940315e5bd14c63
seuyou <3463423099@qq.com>
2021-12-29 19:55:59 +0800

    [Feature]: Customized settings

--- configs/selfsup/mae/mae_vit-b-16-32xb128-coslr-400e-w-norm-decay_in1k.py
+++ configs/selfsup/mae/mae_vit-b-16-32xb128-coslr-400e-w-norm-decay_in1k.py
@@ -9,10 +9,8 @@ _base_ = [
 optimizer = dict(
     lr=1.5e-4 * 4096 / 256,
     paramwise_options={
-        '(bn|gn)(\\d+)?.(weight|bias)': dict(weight_decay=0.),
         'bias': dict(weight_decay=0.),
         'pos_embed': dict(weight_decay=0.),
-        'cls_token': dict(weight_decay=0.),
         'mask_token': dict(weight_decay=0.)
     })
 
@@ -21,29 +19,25 @@ lr_config = dict(
     policy='CosineAnnealing',
     min_lr=1e-5,
     warmup='linear',
-    warmup_iters=40 * 200 // 1600,
+    warmup_iters=40,
     warmup_ratio=1e-4,
-    warmup_by_epoch=True)
+    warmup_by_epoch=True,
+    by_epoch=False)
 
 # runtime settings
 # the max_keep_ckpts controls the max number of ckpt file in your work_dirs
 # if it is 3, when CheckpointHook (in mmcv) saves the 4th ckpt
 # it will remove the oldest one to keep the number of total ckpts as 3
-checkpoint_config = dict(
-    interval=1,
-    max_keep_ckpts=3,
-    out_dir='/mnt/lustre/liuyuan1.vendor/ckpt/mae')
+checkpoint_config = dict(interval=1, max_keep_ckpts=3, out_dir='test_ckpt')
 
 persistent_workers = True
-runner = dict(max_epochs=200)
+runner = dict(max_epochs=400)
 
 log_config = dict(
-    interval=50,
+    interval=1,
     hooks=[
         dict(type='TextLoggerHook'),
         # dict(type='TensorboardLoggerHook'),
     ])
 
-data = dict(imgs_per_gpu=128)
-
-# dist_params = dict(backend='nccl', port=29500)
+data = dict(imgs_per_gpu=1)
