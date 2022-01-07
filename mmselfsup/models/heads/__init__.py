629768202311acc04b7f73effa811fe12cfb41bf
seuyou <3463423099@qq.com>
2022-01-07 11:37:40 +0800

    [Fix]: Fix lint

--- mmselfsup/models/heads/__init__.py
+++ mmselfsup/models/heads/__init__.py
@@ -2,9 +2,9 @@
 from .cls_head import ClsHead
 from .contrastive_head import ContrastiveHead
 from .latent_pred_head import LatentClsHead, LatentPredictHead
-from .soft_cls_head import SoftClsHead
-from .multi_cls_head import MultiClsHead
 from .mse_head import MSEHead
+from .multi_cls_head import MultiClsHead
+from .soft_cls_head import SoftClsHead
 from .swav_head import SwAVHead
 
 __all__ = [
--- run/cls/generate_cmd.py
+++
@@ -1,7 +0,0 @@
-from mae_finetune_100e import *
-
-cmd = f'GPUS_PER_NODE={gpus_per_node} GPUS={gpus} SRUN_ARGS="{srun_args}" \
-    bash tools/benchmarks/classification/slurm_train_linear.sh {partition} \
-    {job_name} {config} {pretrain}'
-
-print(cmd)
