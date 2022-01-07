7c07ab149f4db87cbbfe633433be62a4f9ba183c
seuyou <3463423099@qq.com>
2022-01-07 11:37:40 +0800

    [Fix]: Fix lint

--- run/cls/mae_finetune_100e.py
+++
@@ -1,7 +0,0 @@
-config = 'configs/benchmarks/classification/imagenet/vit-b-16_16xb64-coslr-100e_in1k.py'
-job_name = 'mae_ft_16xb64'
-srun_args = '--quotatype=spot --async'
-partition = 'mm_model'
-pretrain = '/mnt/lustre/liuyuan1.vendor/ckpt/mae/mae_imagenet_pretrain_b4096_400e/epoch_400.pth'
-gpus_per_node = 8
-gpus = 16
--- run/pre-train/mae_b4096_400e.py
+++
@@ -1,7 +0,0 @@
-config = 'configs/selfsup/mae/mae_vit-base_b4096-coslr-400e_in1k.py'
-work_dirs = 'work_dirs/selfsup/mae_imagenet_pretrain_b4096_400e'
-job_name = 'mae_4096_400'
-srun_args = ''
-partition = 'mm_model'
-gpus_per_node = 8
-gpus = 32
