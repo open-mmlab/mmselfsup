edf1c47c5df8485aa72336340c5f3bcd19fe2a5e
seuyou <3463423099@qq.com>
2022-01-10 17:06:26 +0800

    [Feature]: Official MAE finetune

--- mmselfsup/models/heads/__init__.py
+++ mmselfsup/models/heads/__init__.py
@@ -3,12 +3,13 @@ from .cls_head import ClsHead
 from .contrastive_head import ContrastiveHead
 from .latent_pred_head import LatentClsHead, LatentPredictHead
 from .mocov3_head import MoCoV3Head
-from .soft_cls_head import SoftClsHead
 from .multi_cls_head import MultiClsHead
-from .mse_pretrain_head import MAEPretrainHead
+from .mae_pretrain_head import MAEPretrainHead
+from .mae_finetune_head import MAEFinetuneHead
 from .swav_head import SwAVHead
 
 __all__ = [
     'ContrastiveHead', 'ClsHead', 'LatentPredictHead', 'LatentClsHead',
-    'MultiClsHead', 'SwAVHead', 'SoftClsHead', 'MAEPretrainHead', 'MoCoV3Head'
+    'MultiClsHead', 'SwAVHead', 'MAEFinetuneHead', 'MAEPretrainHead',
+    'MoCoV3Head'
 ]
