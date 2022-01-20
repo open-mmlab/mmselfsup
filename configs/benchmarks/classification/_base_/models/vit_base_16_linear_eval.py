47bee6518dbba15bcc4442a3d97fbeb46a420643
seuyou <3463423099@qq.com>
2022-01-20 14:44:47 +0800

    [Fix]: delete norm1

--- configs/benchmarks/classification/_base_/models/vit_base_16_linear_eval.py
+++ configs/benchmarks/classification/_base_/models/vit_base_16_linear_eval.py
@@ -5,5 +5,6 @@ model = dict(
         arch='b',
         patch_size=16,
         global_pool=True,
-        finetune=False),
+        finetune=False,
+        final_norm=False),
     head=dict(type='MAELinearEvalHead', num_classes=1000, embed_dim=768))
