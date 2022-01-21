06b39169e36e97c01b8d54117c1958ad26654689
kaminyou ho <kaminyouho@aetherai.com>
2022-01-21 17:13:59 +0800

    mock bypass

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,8 @@
 import pytest
-from unittest.mock import MagicMock, patch
-import numpy as np
+from unittest.mock import patch
+
 
 @pytest.fixture(scope="session")
 def mock_faiss_in_clutering():
     with patch("mmselfsup.utils.clustering.faiss") as faiss:
-        
         yield faiss
-#def mock_faiss():
-#    mock = MagicMock()
-#    mock.PCAMatrix().apply_py.return_value = np.random.rand(10, 8)
-#    mock.GpuIndexFlatL2().search.return_value = (np.random.rand(1000, 6), np.random.rand(1000, 6))
-#    yield mock
--- tests/test_utils/test_clustering.py
+++ tests/test_utils/test_clustering.py
@@ -9,6 +9,7 @@ def mock_faiss(mock_faiss_in_clutering):
     mock_faiss_in_clutering.PCAMatrix.return_value.apply_py.return_value = np.random.rand(10, 8)
     mock_faiss_in_clutering.GpuIndexFlatL2.return_value.search.return_value = (np.random.rand(1000, 6), np.random.rand(1000, 6))
 
+
 @pytest.mark.parametrize("verbose", [True, False])
 def test_kmeans(mock_faiss, verbose):
     fake_input = np.random.rand(10, 8).astype(np.float32)
