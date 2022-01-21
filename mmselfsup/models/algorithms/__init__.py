a6bcb17381200994f8cc53e1381a268b1c898f97
seuyou <3463423099@qq.com>
2022-01-21 19:06:11 +0800

    [Feature]: Add docstring

--- mmselfsup/models/algorithms/__init__.py
+++ mmselfsup/models/algorithms/__init__.py
@@ -5,6 +5,8 @@ from .classification import Classification
 from .deepcluster import DeepCluster
 from .densecl import DenseCL
 from .mae import MAE
+from .mae_finetune import VitClassification
+from .mae_linprobe import MAELinearClassification
 from .moco import MoCo
 from .mocov3 import MoCoV3
 from .npid import NPID
@@ -14,8 +16,6 @@ from .rotation_pred import RotationPred
 from .simclr import SimCLR
 from .simsiam import SimSiam
 from .swav import SwAV
-from .mae_finetune import VitClassification
-from .mae_linprobe import MAELinearClassification
 
 __all__ = [
     'BaseModel', 'BYOL', 'Classification', 'DeepCluster', 'DenseCL', 'MoCo',
