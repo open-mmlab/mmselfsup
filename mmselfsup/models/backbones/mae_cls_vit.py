b4da297deffaa5a84443fbba17d27bacc13c4a81
seuyou <3463423099@qq.com>
2022-01-21 19:06:11 +0800

    [Feature]: Add docstring

--- mmselfsup/models/backbones/mae_cls_vit.py
+++ mmselfsup/models/backbones/mae_cls_vit.py
@@ -8,6 +8,38 @@ from ..builder import BACKBONES
 
 @BACKBONES.register_module()
 class MAEClsViT(VisionTransformer):
+    """Vision Transformer for MAE classification (fine-tuning or linear probe).
+
+    A PyTorch implement of : `An Image is Worth 16x16 Words: Transformers
+    for Image Recognition at Scale <https://arxiv.org/abs/2010.11929>`_
+
+    Args:
+        arch (str | dict): Vision Transformer architecture
+            Default: 'b'
+        img_size (int | tuple): Input image size
+        patch_size (int | tuple): The patch size
+        out_indices (Sequence | int): Output from which stages.
+            Defaults to -1, means the last stage.
+        drop_rate (float): Probability of an element to be zeroed.
+            Defaults to 0.
+        drop_path_rate (float): stochastic depth rate. Defaults to 0.
+        norm_cfg (dict): Config dict for normalization layer.
+            Defaults to ``dict(type='LN')``.
+        final_norm (bool): Whether to add a additional layer to normalize
+            final feature map. Defaults to True.
+        output_cls_token (bool): Whether output the cls_token. If set True,
+            `with_cls_token` must be True. Defaults to True.
+        interpolate_mode (str): Select the interpolate mode for position
+            embeding vector resize. Defaults to "bicubic".
+        patch_cfg (dict): Configs of patch embeding. Defaults to an empty dict.
+        layer_cfgs (Sequence | dict): Configs of each transformer layer in
+            encoder. Defaults to an empty dict.
+        global_pool (bool): Whether or not use the global_pooling feature
+            for classification. Defaults to True.
+        finetune (bool): Whether or not do fine-tuning. Defaults to True.
+        init_cfg (dict, optional): Initialization config dict.
+            Defaults to None.
+    """
 
     def __init__(self,
                  arch='b',
