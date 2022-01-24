db832645f133de9428699bf6395cb7cf3a8a8af2
UnoWu <st9007a@gmail.com>
2022-01-24 13:55:48 +0800

    Fix test data

--- tests/test_data/test_datasets/test_builder.py
+++ tests/test_data/test_datasets/test_builder.py
@@ -3,14 +3,33 @@ import pytest
 from mmselfsup.datasets import (ConcatDataset, MultiViewDataset, RepeatDataset,
                                 build_dataset)
 
-DATASET_CONFIG = {
-    'type': 'MultiViewDataset',
-    'data_source': {
-        'type': 'ImageNet',
-        'data_prefix': 'data/imagenet/train',
-        'ann_file': 'data/imagenet/meta/train.txt',
-    },
-}
+DATASET_CONFIG = dict(
+    type='MultiViewDataset',
+    data_source=dict(
+        type='ImageNet',
+        data_prefix='data/imagenet/train',
+        ann_file='data/imagenet/meta/train.txt',
+    ),
+    num_views=[1, 1],
+    pipeline=[
+        dict(type='RandomResizedCrop', size=224, interpolation=3),
+        dict(type='RandomHorizontalFlip'),
+        dict(
+            type='RandomAppliedTrans',
+            transforms=[
+                dict(
+                    type='ColorJitter',
+                    brightness=0.4,
+                    contrast=0.4,
+                    saturation=0.2,
+                    hue=0.1)
+            ],
+            p=0.8),
+        dict(type='RandomGrayscale', p=0.2),
+        dict(type='GaussianBlur', sigma_min=0.1, sigma_max=2.0, p=1.),
+        dict(type='Solarization', p=0.),
+    ],
+)
 
 
 @pytest.mark.parametrize('cfg, expected_type', [
