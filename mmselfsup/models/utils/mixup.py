145772a546ad0b96c62033b41db0d43f8869accf
seuyou <3463423099@qq.com>
2022-01-25 15:45:33 +0800

    [Feature]: Add unit test for remaining parts

--- mmselfsup/models/utils/mixup.py
+++ mmselfsup/models/utils/mixup.py
@@ -147,34 +147,6 @@ class Mixup:
         self.correct_lam = correct_lam
         self.mixup_enabled = True
 
-    def _params_per_elem(self, batch_size):
-        lam = np.ones(batch_size, dtype=np.float32)
-        use_cutmix = np.zeros(batch_size, dtype=np.bool)
-        if self.mixup_enabled:
-            if self.mixup_alpha > 0. and self.cutmix_alpha > 0.:
-                use_cutmix = np.random.rand(batch_size) < self.switch_prob
-                lam_mix = np.where(
-                    use_cutmix,
-                    np.random.beta(
-                        self.cutmix_alpha, self.cutmix_alpha, size=batch_size),
-                    np.random.beta(
-                        self.mixup_alpha, self.mixup_alpha, size=batch_size))
-            elif self.mixup_alpha > 0.:
-                lam_mix = np.random.beta(
-                    self.mixup_alpha, self.mixup_alpha, size=batch_size)
-            elif self.cutmix_alpha > 0.:
-                use_cutmix = np.ones(batch_size, dtype=np.bool)
-                lam_mix = np.random.beta(
-                    self.cutmix_alpha, self.cutmix_alpha, size=batch_size)
-            else:
-                assert False, 'One of mixup_alpha > 0., cutmix_alpha > 0., \
-                    cutmix_minmax not None should be true.'
-
-            lam = np.where(
-                np.random.rand(batch_size) < self.mix_prob,
-                lam_mix.astype(np.float32), lam)
-        return lam, use_cutmix
-
     def _params_per_batch(self):
         lam = 1.
         use_cutmix = False
@@ -197,53 +169,6 @@ class Mixup:
             lam = float(lam_mix)
         return lam, use_cutmix
 
-    def _mix_elem(self, x):
-        batch_size = len(x)
-        lam_batch, use_cutmix = self._params_per_elem(batch_size)
-        x_orig = x.clone(
-        )  # need to keep an unmodified original for mixing source
-        for i in range(batch_size):
-            j = batch_size - i - 1
-            lam = lam_batch[i]
-            if lam != 1.:
-                if use_cutmix[i]:
-                    (yl, yh, xl, xh), lam = cutmix_bbox_and_lam(
-                        x[i].shape,
-                        lam,
-                        ratio_minmax=self.cutmix_minmax,
-                        correct_lam=self.correct_lam)
-                    x[i][:, yl:yh, xl:xh] = x_orig[j][:, yl:yh, xl:xh]
-                    lam_batch[i] = lam
-                else:
-                    x[i] = x[i] * lam + x_orig[j] * (1 - lam)
-        return torch.tensor(
-            lam_batch, device=x.device, dtype=x.dtype).unsqueeze(1)
-
-    def _mix_pair(self, x):
-        batch_size = len(x)
-        lam_batch, use_cutmix = self._params_per_elem(batch_size // 2)
-        x_orig = x.clone(
-        )  # need to keep an unmodified original for mixing source
-        for i in range(batch_size // 2):
-            j = batch_size - i - 1
-            lam = lam_batch[i]
-            if lam != 1.:
-                if use_cutmix[i]:
-                    (yl, yh, xl, xh), lam = cutmix_bbox_and_lam(
-                        x[i].shape,
-                        lam,
-                        ratio_minmax=self.cutmix_minmax,
-                        correct_lam=self.correct_lam)
-                    x[i][:, yl:yh, xl:xh] = x_orig[j][:, yl:yh, xl:xh]
-                    x[j][:, yl:yh, xl:xh] = x_orig[i][:, yl:yh, xl:xh]
-                    lam_batch[i] = lam
-                else:
-                    x[i] = x[i] * lam + x_orig[j] * (1 - lam)
-                    x[j] = x[j] * lam + x_orig[i] * (1 - lam)
-        lam_batch = np.concatenate((lam_batch, lam_batch[::-1]))
-        return torch.tensor(
-            lam_batch, device=x.device, dtype=x.dtype).unsqueeze(1)
-
     def _mix_batch(self, x):
         lam, use_cutmix = self._params_per_batch()
         if lam == 1.:
@@ -262,12 +187,7 @@ class Mixup:
 
     def __call__(self, x, target):
         assert len(x) % 2 == 0, 'Batch size should be even when using this'
-        if self.mode == 'elem':
-            lam = self._mix_elem(x)
-        elif self.mode == 'pair':
-            lam = self._mix_pair(x)
-        else:
-            lam = self._mix_batch(x)
+        lam = self._mix_batch(x)
         target = mixup_target(target, self.num_classes, lam,
                               self.label_smoothing)
         return x, target
---
+++ tests/test_models/test_backbones/test_mae_cls_vit.py
@@ -0,0 +1,44 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+import torch
+
+from mmselfsup.models.backbones import MAEClsViT
+
+finetune_backbone = dict(
+    arch='b',
+    patch_size=16,
+    global_pool=True,
+    drop_path_rate=0.1,
+    final_norm=False)
+
+finetune_backbone_norm = dict(
+    arch='b',
+    patch_size=16,
+    global_pool=False,
+    drop_path_rate=0.1,
+    final_norm=True)
+
+linprobe_backbone = dict(
+    arch='b',
+    patch_size=16,
+    global_pool=True,
+    finetune=False,
+    final_norm=False)
+
+
+def test_mae_cls_vit():
+    mae_finetune_backbone = MAEClsViT(**finetune_backbone)
+    mae_finetune_backbone_norm = MAEClsViT(**finetune_backbone_norm)
+    mae_linprobe_backbone = MAEClsViT(**linprobe_backbone)
+    mae_linprobe_backbone.apply(mae_linprobe_backbone._init_weights)
+    mae_linprobe_backbone.train()
+
+    fake_inputs = torch.randn((2, 3, 224, 224))
+    fake_finetune_outputs = mae_finetune_backbone(fake_inputs)
+    fake_finetune_outputs_norm = mae_finetune_backbone_norm(fake_inputs)
+    fake_linprobe_outputs = mae_linprobe_backbone(fake_inputs)
+    assert list(fake_finetune_outputs.shape) == [2, 768]
+    assert list(fake_linprobe_outputs.shape) == [2, 768]
+    assert list(fake_finetune_outputs_norm.shape) == [2, 768]
+
+
+test_mae_cls_vit()
