bff04c78f47d0ae3c2119fad48446a7f243bd7ae
liming <liming.ai@bytedance.com>
2022-01-25 09:59:48 +0800

    fix bug when loading cifar dataset

--- mmselfsup/datasets/data_sources/base.py
+++ mmselfsup/datasets/data_sources/base.py
@@ -83,11 +83,22 @@ class BaseDataSource(object, metaclass=ABCMeta):
         if self.file_client is None:
             self.file_client = mmcv.FileClient(**self.file_client_args)
 
-        if self.data_infos[idx]['img_prefix'] is not None:
-            filename = osp.join(self.data_infos[idx]['img_prefix'],
-                                self.data_infos[idx]['img_info']['filename'])
+        if self.data_infos[idx].get('img_prefix', None) is not None:
+            if self.data_infos[idx]['img_prefix'] is not None:
+                filename = osp.join(self.data_infos[idx]['img_prefix'],
+                                    self.data_infos[idx]['img_info']['filename'])
+            else:
+                filename = self.data_infos[idx]['img_info']['filename']
+
+            img_bytes = self.file_client.get(filename)
+            img = mmcv.imfrombytes(img_bytes, flag=self.color_type)
+        # cifar pictures has been loaded to memory in mmselfsup/datasets/data_sources/cifar.py
+        # it is no need to load again and the key for cifar image is 'img'
         else:
-            filename = self.data_infos[idx]['img_info']['filename']
+            img = self.data_infos[idx]['img']
+
+        img = img.astype(np.uint8)
+        return Image.fromarray(img)
 
         img_bytes = self.file_client.get(filename)
         img = mmcv.imfrombytes(img_bytes, flag=self.color_type)
