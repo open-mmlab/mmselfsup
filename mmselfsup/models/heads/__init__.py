15dc0b93858af8e45c20a5733125aedfda90104d
fangyixiao18 <fangyx18@hotmail.com>
2022-01-26 17:23:43 +0800

    [Feature] add mocov3 head

--- mmselfsup/models/heads/__init__.py
+++ mmselfsup/models/heads/__init__.py
@@ -2,10 +2,11 @@
 from .cls_head import ClsHead
 from .contrastive_head import ContrastiveHead
 from .latent_pred_head import LatentClsHead, LatentPredictHead
+from .mocov3_head import MoCov3Head
 from .multi_cls_head import MultiClsHead
 from .swav_head import SwAVHead
 
 __all__ = [
     'ContrastiveHead', 'ClsHead', 'LatentPredictHead', 'LatentClsHead',
-    'MultiClsHead', 'SwAVHead'
+    'MoCov3Head', 'MultiClsHead', 'SwAVHead'
 ]
