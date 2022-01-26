8396a752e849ffe83292d57563813894c76f8805
fangyixiao18 <fangyx18@hotmail.com>
2022-01-26 19:50:59 +0800

    [Feature] remove duplicated codes

--- mmselfsup/models/backbones/vision_transformer.py
+++ mmselfsup/models/backbones/vision_transformer.py
@@ -105,35 +105,6 @@ class VisionTransformer(_VisionTransformer):
                     nn.init.zeros_(m.bias)
             nn.init.normal_(self.cls_token, std=1e-6)
 
-    def build_2d_sincos_position_embedding(self, temperature=10000.):
-        """The function is to build position embedding for model to obtain the
-        position information of the image patches."""
-        h, w = self.patch_embed.patches_resolution
-        grid_w = torch.arange(w, dtype=torch.float32)
-        grid_h = torch.arange(h, dtype=torch.float32)
-        grid_w, grid_h = torch.meshgrid(grid_w, grid_h)
-        assert self.embed_dims % 4 == 0, \
-            'Embed dimension must be divisible by 4.'
-        pos_dim = self.embed_dims // 4
-
-        omega = torch.arange(pos_dim, dtype=torch.float32) / pos_dim
-        omega = 1. / (temperature**omega)
-        out_w = torch.einsum('m,d->md', [grid_w.flatten(), omega])
-        out_h = torch.einsum('m,d->md', [grid_h.flatten(), omega])
-        pos_emb = torch.cat(
-            [
-                torch.sin(out_w),
-                torch.cos(out_w),
-                torch.sin(out_h),
-                torch.cos(out_h)
-            ],
-            dim=1,
-        )[None, :, :]
-
-        pe_token = torch.zeros([1, 1, self.embed_dims], dtype=torch.float32)
-        self.pos_embed.data.copy_(torch.cat([pe_token, pos_emb], dim=1))
-        self.pos_embed.requires_grad = False
-
     def _freeze_stages(self):
         """Freeze patch_embed layer, some parameters and stages."""
         if self.frozen_stages >= 0:
