8cae4533e9de8d822d385da767a88825f05b93c1
fangyixiao18 <fangyx18@hotmail.com>
2022-01-26 17:23:05 +0800

    [Feature] add position embedding function

--- mmselfsup/utils/__init__.py
+++ mmselfsup/utils/__init__.py
@@ -7,12 +7,15 @@ from .distributed_sinkhorn import distributed_sinkhorn
 from .extractor import Extractor
 from .gather import concat_all_gather, gather_tensors, gather_tensors_batch
 from .logger import get_root_logger
+from .position_embedding import build_2d_sincos_position_embedding
+from .setup_env import setup_multi_processes
 from .test_helper import multi_gpu_test, single_gpu_test
 
 __all__ = [
     'AliasMethod', 'batch_shuffle_ddp', 'batch_unshuffle_ddp',
     'dist_forward_collect', 'nondist_forward_collect', 'collect_env',
     'distributed_sinkhorn', 'Extractor', 'concat_all_gather', 'gather_tensors',
-    'gather_tensors_batch', 'get_root_logger', 'multi_gpu_test',
-    'single_gpu_test'
+    'gather_tensors_batch', 'get_root_logger',
+    'build_2d_sincos_position_embedding', 'setup_multi_processes',
+    'multi_gpu_test', 'single_gpu_test'
 ]
---
+++ mmselfsup/utils/position_embedding.py
@@ -0,0 +1,33 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+import torch
+
+
+def build_2d_sincos_position_embedding(patches_resolution,
+                                       embed_dims,
+                                       temperature=10000.):
+    """The function is to build position embedding for model to obtain the
+        position information of the image patches."""
+    h, w = patches_resolution
+    grid_w = torch.arange(w, dtype=torch.float32)
+    grid_h = torch.arange(h, dtype=torch.float32)
+    grid_w, grid_h = torch.meshgrid(grid_w, grid_h)
+    assert embed_dims % 4 == 0, \
+        'Embed dimension must be divisible by 4.'
+    pos_dim = embed_dims // 4
+
+    omega = torch.arange(pos_dim, dtype=torch.float32) / pos_dim
+    omega = 1. / (temperature**omega)
+    out_w = torch.einsum('m,d->md', [grid_w.flatten(), omega])
+    out_h = torch.einsum('m,d->md', [grid_h.flatten(), omega])
+
+    pos_emb = torch.cat(
+        [
+            torch.sin(out_w),
+            torch.cos(out_w),
+            torch.sin(out_h),
+            torch.cos(out_h)
+        ],
+        dim=1,
+    )[None, :, :]
+    pe_token = torch.zeros([1, 1, embed_dims], dtype=torch.float32)
+    return pe_token, pos_emb
