f607d5c4b4ecbbd25ae045de18f2f0243a70bf0e
seuyou <3463423099@qq.com>
2022-01-26 19:53:41 +0800

    [Fix]: Change norm_pix_loss to norm_pix in pretrain head

--- configs/selfsup/_base_/models/mae.py
+++ configs/selfsup/_base_/models/mae.py
@@ -12,4 +12,4 @@ model = dict(
         decoder_num_heads=16,
         mlp_ratio=4.,
     ),
-    head=dict(type='MAEPretrainHead', norm_pix_loss=False, patch_size=16))
+    head=dict(type='MAEPretrainHead', norm_pix=False, patch_size=16))
