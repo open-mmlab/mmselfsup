8f192cb531b96aed6d29bf823ff182cf5d3bbaee
fangyixiao18 <fangyx18@hotmail.com>
2022-01-26 17:28:34 +0800

    [Feature] add mocov3 algorithm

--- mmselfsup/models/algorithms/__init__.py
+++ mmselfsup/models/algorithms/__init__.py
@@ -5,6 +5,7 @@ from .classification import Classification
 from .deepcluster import DeepCluster
 from .densecl import DenseCL
 from .moco import MoCo
+from .mocov3 import MoCov3
 from .npid import NPID
 from .odc import ODC
 from .relative_loc import RelativeLoc
@@ -15,5 +16,6 @@ from .swav import SwAV
 
 __all__ = [
     'BaseModel', 'BYOL', 'Classification', 'DeepCluster', 'DenseCL', 'MoCo',
-    'NPID', 'ODC', 'RelativeLoc', 'RotationPred', 'SimCLR', 'SimSiam', 'SwAV'
+    'MoCov3', 'NPID', 'ODC', 'RelativeLoc', 'RotationPred', 'SimCLR',
+    'SimSiam', 'SwAV'
 ]
