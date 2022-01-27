7664df6990cfe2ec88fdc42aa95a6e8ef435459e
seuyou <3463423099@qq.com>
2022-01-27 21:38:37 +0800

    [Fix]: Add False to finetune mae linprobe‘ “

--- configs/benchmarks/classification/_base_/models/vit-base-p16_linprobe.py
+++ configs/benchmarks/classification/_base_/models/vit-base-p16_linprobe.py
@@ -1,4 +1,9 @@
 model = dict(
     type='Classification',
-    backbone=dict(type='MAEClsViT', arch='b', patch_size=16, final_norm=False),
+    backbone=dict(
+        type='MAEClsViT',
+        arch='b',
+        patch_size=16,
+        final_norm=False,
+        finetune=False),
     head=dict(type='MAELinprobeHead', num_classes=1000, embed_dim=768))
