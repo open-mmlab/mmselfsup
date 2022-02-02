e6ce5934c2e1b5058989a58a55018f48d04134bf
DavidChenL <3439388084@qq.com>
2022-02-02 16:18:05 +0800

    [Fix] fix deepcluster base config error 'resnrt'

--- configs/selfsup/deepcluster/deepcluster-sobel_resnet50_8xb64-steplr-200e_in1k.py
+++ configs/selfsup/deepcluster/deepcluster-sobel_resnet50_8xb64-steplr-200e_in1k.py
@@ -1,4 +1,4 @@
-_base_ = 'deepcluster_resnrt50_8xb64-steplr-200e_in1k.py'
+_base_ = 'deepcluster_resnet50_8xb64-steplr-200e_in1k.py'
 
 # model settings
 model = dict(with_sobel=True, backbone=dict(in_channels=2))
