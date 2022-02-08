5dc0004e45d62288e41599f52f224532e9b85dab
xcnick <xcnick0412@gmail.com>
2022-02-08 13:55:11 +0800

    Add barlowtwins

---
+++ configs/selfsup/barlowtwins/barlowtwins_resnet50_8xb32-accum8-coslr-200e_in1k.py
@@ -0,0 +1,29 @@
+_base_ = [
+    '../_base_/models/barlowtwins.py',
+    '../_base_/datasets/imagenet_byol.py',
+    '../_base_/schedules/lars_coslr-200e_in1k.py',
+    '../_base_/default_runtime.py',
+]
+
+# additional hooks
+# interval for accumulate gradient, total 8*32*8(interval)=2048
+update_interval = 8
+
+# optimizer
+optimizer = dict(
+    type='LARS',
+    lr=4.8,
+    momentum=0.9,
+    weight_decay=1e-6,
+    paramwise_options={
+        '(bn|gn)(\\d+)?.(weight|bias)':
+        dict(weight_decay=0., lars_exclude=True),
+        'bias': dict(weight_decay=0., lars_exclude=True)
+    })
+optimizer_config = dict(update_interval=update_interval)
+
+# runtime settings
+# the max_keep_ckpts controls the max number of ckpt file in your work_dirs
+# if it is 3, when CheckpointHook (in mmcv) saves the 4th ckpt
+# it will remove the oldest one to keep the number of total ckpts as 3
+checkpoint_config = dict(interval=10, max_keep_ckpts=3)
