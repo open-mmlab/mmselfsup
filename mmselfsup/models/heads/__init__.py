cd367931940c7c076ecfd0054f0da2142bab3ebb
seuyou <3463423099@qq.com>
2022-02-09 19:23:04 +0800

    [Fix]: Fix isort

--- mmselfsup/models/heads/__init__.py
+++ mmselfsup/models/heads/__init__.py
@@ -2,11 +2,11 @@
 from .cls_head import ClsHead
 from .contrastive_head import ContrastiveHead
 from .latent_pred_head import LatentClsHead, LatentPredictHead
-from .mocov3_head import MoCoV3Head
-from .multi_cls_head import MultiClsHead
-from .mae_pretrain_head import MAEPretrainHead
 from .mae_finetune_head import MAEFinetuneHead
 from .mae_linprobe_head import MAELinprobeHead
+from .mae_pretrain_head import MAEPretrainHead
+from .mocov3_head import MoCoV3Head
+from .multi_cls_head import MultiClsHead
 from .swav_head import SwAVHead
 
 __all__ = [
