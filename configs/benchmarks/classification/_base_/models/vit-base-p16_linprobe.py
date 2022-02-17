0d0a49fceaa985dedc33635a3769e923f3b6e921
seuyou <3463423099@qq.com>
2022-02-17 10:06:14 +0800

    [Fix]: Use cls_token to linear prob

--- configs/benchmarks/classification/_base_/models/vit-base-p16_linprobe.py
+++ configs/benchmarks/classification/_base_/models/vit-base-p16_linprobe.py
@@ -4,6 +4,6 @@ model = dict(
         type='MIMVisionTransformer',
         arch='b',
         patch_size=16,
-        final_norm=False,
+        final_norm=True,
         finetune=False),
     head=dict(type='MAELinprobeHead', num_classes=1000, embed_dim=768))
