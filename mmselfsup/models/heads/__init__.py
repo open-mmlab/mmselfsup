439834b2296305024706df23a9152ceecc1ca9ae
seuyou <3463423099@qq.com>
2022-02-24 13:23:54 +0800

    [Fix]: Fix import error in __init__.py

--- mmselfsup/models/heads/__init__.py
+++ mmselfsup/models/heads/__init__.py
@@ -2,7 +2,7 @@
 from .cls_head import ClsHead
 from .contrastive_head import ContrastiveHead
 from .latent_pred_head import LatentClsHead, LatentPredictHead
-from .mae_pretrain_head import MAEFinetuneHead, MAEPretrainHead
+from .mae_head import MAEFinetuneHead, MAEPretrainHead
 from .mocov3_head import MoCoV3Head
 from .multi_cls_head import MultiClsHead
 from .swav_head import SwAVHead
