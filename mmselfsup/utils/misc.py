5499606eb4a1ace629e34af4a4dbf2c617d890b3
Yixiao Fang <36138628+fangyixiao18@users.noreply.github.com>
2022-03-25 13:32:10 +0800

    [Feature] Resume from the latest checkpoint automatically. (#245)
    * [Feature] Resume from the latest checkpoint automatically.
    
    * fix windows path problem
    
    * fix lint
    
    * add code reference

--- mmselfsup/utils/misc.py
+++ mmselfsup/utils/misc.py
@@ -1,4 +1,8 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+import glob
+import os.path as osp
+import warnings
+
 import mmcv
 import numpy as np
 
@@ -15,3 +19,37 @@ def tensor2imgs(tensor, mean=(0, 0, 0), std=(1, 1, 1), to_rgb=True):
             img, mean, std, to_bgr=to_rgb).astype(np.uint8)
         imgs.append(np.ascontiguousarray(img))
     return imgs
+
+
+def find_latest_checkpoint(path, suffix='pth'):
+    """Find the latest checkpoint from the working directory.
+    Args:
+        path(str): The path to find checkpoints.
+        suffix(str): File extension.
+            Defaults to pth.
+    Returns:
+        latest_path(str | None): File path of the latest checkpoint.
+    References:
+        .. [1] https://github.com/microsoft/SoftTeacher
+                    /blob/main/ssod/utils/patch.py
+        .. [2] https://github.com/open-mmlab/mmdetection
+                    /blob/master/mmdet/utils/misc.py#L7
+    """
+    if not osp.exists(path):
+        warnings.warn('The path of checkpoints does not exist.')
+        return None
+    if osp.exists(osp.join(path, f'latest.{suffix}')):
+        return osp.join(path, f'latest.{suffix}')
+
+    checkpoints = glob.glob(osp.join(path, f'*.{suffix}'))
+    if len(checkpoints) == 0:
+        warnings.warn('There are no checkpoints in the path.')
+        return None
+    latest = -1
+    latest_path = None
+    for checkpoint in checkpoints:
+        count = int(osp.basename(checkpoint).split('_')[-1].split('.')[0])
+        if count > latest:
+            latest = count
+            latest_path = checkpoint
+    return latest_path
