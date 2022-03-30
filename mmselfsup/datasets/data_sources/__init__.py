70b9b8ba389ea18fe0cbd278ddfcb10f697e6bee
Ming Li <73068772+mitming@users.noreply.github.com>
2022-03-30 20:07:59 +0800

    [Feature] Add ImageNet21k (#225)
    * solve memory leak by limited implementation
    
    * fix lint problem
    
    Co-authored-by: liming <liming.ai@bytedance.com>

--- mmselfsup/datasets/data_sources/__init__.py
+++ mmselfsup/datasets/data_sources/__init__.py
@@ -3,5 +3,9 @@ from .base import BaseDataSource
 from .cifar import CIFAR10, CIFAR100
 from .image_list import ImageList
 from .imagenet import ImageNet
+from .imagenet_21k import ImageNet21k
 
-__all__ = ['BaseDataSource', 'CIFAR10', 'CIFAR100', 'ImageList', 'ImageNet']
+__all__ = [
+    'BaseDataSource', 'CIFAR10', 'CIFAR100', 'ImageList', 'ImageNet',
+    'ImageNet21k'
+]
