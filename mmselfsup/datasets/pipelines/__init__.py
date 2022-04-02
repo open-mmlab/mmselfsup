971e31eb1cd65931ecc96dd728b6810d6386e965
YuanLiuuuuuu <3463423099@qq.com>
2022-04-03 00:33:02 +0800

    [Feature]: Add dataset of CAE

--- mmselfsup/datasets/pipelines/__init__.py
+++ mmselfsup/datasets/pipelines/__init__.py
@@ -1,8 +1,8 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .transforms import (BlockwiseMaskGenerator, GaussianBlur, Lighting,
-                         RandomAppliedTrans, RandomAug, Solarization)
+                         RandomAppliedTrans, RandomAug, Solarization, ToTensor)
 
 __all__ = [
     'GaussianBlur', 'Lighting', 'RandomAppliedTrans', 'Solarization',
-    'RandomAug', 'BlockwiseMaskGenerator'
+    'RandomAug', 'BlockwiseMaskGenerator', 'ToTensor'
 ]
