e7e3fbb96a90b5ae8090a0f8ec8a68aeea4ba804
YuanLiuuuuuu <3463423099@qq.com>
2022-04-09 21:10:29 +0800

    [Fix]: Fix cae ft config

--- configs/benchmarks/classification/imagenet/vit-base-p16_ft-8xb128-coslr-100e-rpe_in1k.py
+++ configs/benchmarks/classification/imagenet/vit-base-p16_ft-8xb128-coslr-100e-rpe_in1k.py
@@ -1,4 +1,12 @@
 _base_ = 'vit-b-p16_ft-8xb128-coslr-100e_in1k.py'
 
 # model
-model = dict(backbone=dict(use_window=True))
+model = dict(backbone=dict(use_window=True, init_values=0.1))
+
+# optimizer
+optimizer = dict(lr=8e-3)
+
+# learning policy
+lr_config = dict(warmup_iters=10)
+
+find_unused_parameters = True
