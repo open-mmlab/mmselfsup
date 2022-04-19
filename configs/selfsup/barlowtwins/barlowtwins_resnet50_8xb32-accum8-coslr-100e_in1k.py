a6f4efc92b4f8a7e57ab746faaef8d428c30ad56
fangyixiao18 <fangyx18@hotmail.com>
2022-04-19 20:29:15 +0800

    fix batch size bug

--- configs/selfsup/barlowtwins/barlowtwins_resnet50_8xb32-accum8-coslr-100e_in1k.py
+++ configs/selfsup/barlowtwins/barlowtwins_resnet50_8xb32-accum8-coslr-100e_in1k.py
@@ -1,5 +1,7 @@
 _base_ = 'barlowtwins_resnet50_8xb256-coslr-300e_in1k.py'
 
+data = dict(samples_per_gpu=32)
+
 # additional hooks
 # interval for accumulate gradient, total 8*32*8(interval)=2048
 update_interval = 8
