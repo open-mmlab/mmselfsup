7a77e9b58e18ae2f21375a078b2b74ea4788430e
fangyixiao18 <fangyx18@hotmail.com>
2022-04-19 10:56:33 +0800

    revise LatentCrossCorrelationHead

--- mmselfsup/models/heads/latent_pred_head.py
+++ mmselfsup/models/heads/latent_pred_head.py
@@ -1,7 +1,7 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import torch
 import torch.nn as nn
-from mmcv.runner import BaseModule
+from mmcv.runner import BaseModule, get_dist_info
 
 from ..builder import HEADS, build_neck
 
@@ -78,7 +78,9 @@ class LatentClsHead(BaseModule):
 
 @HEADS.register_module()
 class LatentCrossCorrelationHead(BaseModule):
-    """Head for latent feature cross correlation.
+    """Head for latent feature cross correlation. Part of the code is borrowed
+    from:
+    `https://github.com/facebookresearch/barlowtwins/blob/main/main.py>`_.
 
     Args:
         in_channels (int): Number of input channels.
@@ -87,6 +89,7 @@ class LatentCrossCorrelationHead(BaseModule):
     def __init__(self, in_channels, lambd=0.0051):
         super(LatentCrossCorrelationHead, self).__init__()
         self.lambd = lambd
+        _, self.world_size = get_dist_info()
         self.bn = nn.BatchNorm1d(in_channels, affine=False)
 
     def forward(self, input, target):
@@ -99,17 +102,23 @@ class LatentCrossCorrelationHead(BaseModule):
         Returns:
             dict[str, Tensor]: A dictionary of loss components.
         """
+        # cross-correlation matrix
+        cross_correlation_matrix = self.bn(input).T @ self.bn(target)
+        cross_correlation_matrix.div_(input.size(0) * self.world_size)
 
-        def off_diagonal(x):
-            # return a flattened view of the off-diagonal elements of
-            # a square matrix
-            n, m = x.shape
-            assert n == m
-            return x.flatten()[:-1].view(n - 1, n + 1)[:, 1:].flatten()
+        if torch.distributed.is_initialized():
+            torch.distributed.all_reduce(cross_correlation_matrix)
 
-        c = self.bn(input).T @ self.bn(target)
-        c.div_(input.size(0))
-        on_diag = torch.diagonal(c).add_(-1).pow_(2).sum()
-        off_diag = off_diagonal(c).pow_(2).sum()
+        # loss
+        on_diag = torch.diagonal(cross_correlation_matrix).add_(-1).pow_(
+            2).sum()
+        off_diag = self.off_diagonal(cross_correlation_matrix).pow_(2).sum()
         loss = on_diag + self.lambd * off_diag
         return dict(loss=loss)
+
+    def off_diagonal(self, x):
+        """Rreturn a flattened view of the off-diagonal elements of a square
+        matrix."""
+        n, m = x.shape
+        assert n == m
+        return x.flatten()[:-1].view(n - 1, n + 1)[:, 1:].flatten()
