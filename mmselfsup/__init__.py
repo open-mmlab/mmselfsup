6567ace4bf9fffccf3d4a4d30ca05bcae6b7ba62
YuanLiuuuuuu <3463423099@qq.com>
2022-04-29 15:18:48 +0800

    [Feature]: Add version constrain for mmcls

--- mmselfsup/__init__.py
+++ mmselfsup/__init__.py
@@ -1,6 +1,7 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import warnings
 
+import mmcls
 import mmcv
 from packaging.version import parse
 
@@ -51,10 +52,17 @@ mmcv_minimum_version = '1.3.16'
 mmcv_maximum_version = '1.5.0'
 mmcv_version = digit_version(mmcv.__version__)
 
+mmcls_minumum_version = '0.21.0'
+mmcls_version = digit_version(mmcls.__version__)
+
 
 assert (mmcv_version >= digit_version(mmcv_minimum_version)
         and mmcv_version <= digit_version(mmcv_maximum_version)), \
     f'MMCV=={mmcv.__version__} is used but incompatible. ' \
     f'Please install mmcv>={mmcv_minimum_version}, <={mmcv_maximum_version}.'
+
+assert mmcls_version >= digit_version(mmcv_minimum_version), \
+    f'MMClassification=={mmcls.__version__} is used but incompatible. ' \
+    f'Please install mmcls>={mmcv_minimum_version}.'
 
 __all__ = ['__version__', 'digit_version']
