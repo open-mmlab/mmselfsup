44ff7a84f4558b874cd575e7c391d4b534cb4a9d
fangyixiao.vendor <fangyixiao@pjlab.org.cn>
2022-05-13 07:55:46 +0000

    [Refactor] refactor train/test interface

--- mmselfsup/apis/__init__.py
+++
@@ -1,4 +0,0 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-from .train import init_random_seed, set_random_seed, train_model
-
-__all__ = ['init_random_seed', 'set_random_seed', 'train_model']
--- tools/test.py
+++ tools/test.py
@@ -2,18 +2,11 @@
 import argparse
 import os
 import os.path as osp
-import time
 
-import mmcv
-import torch
-from mmcv import DictAction
-from mmcv.parallel import MMDataParallel, MMDistributedDataParallel
-from mmcv.runner import get_dist_info, init_dist, load_checkpoint
+from mmengine.config import Config, DictAction
+from mmengine.runner import Runner
 
-from mmselfsup.datasets import build_dataloader, build_dataset
-from mmselfsup.models import build_algorithm
-from mmselfsup.utils import (get_root_logger, multi_gpu_test,
-                             setup_multi_processes, single_gpu_test)
+from mmselfsup.utils import register_all_modules
 
 
 def parse_args():
@@ -21,22 +14,12 @@ def parse_args():
         description='MMSelfSup test (and eval) a model')
     parser.add_argument('config', help='test config file path')
     parser.add_argument('checkpoint', help='checkpoint file')
-    parser.add_argument(
-        '--work_dir',
-        help='(Deprecated, please use --work-dir) the dir to save logs and '
-        'models')
     parser.add_argument('--work-dir', help='the dir to save logs and models')
     parser.add_argument(
         '--launcher',
         choices=['none', 'pytorch', 'slurm', 'mpi'],
         default='none',
         help='job launcher')
-    parser.add_argument(
-        '--gpu-id',
-        type=int,
-        default=0,
-        help='id of gpu to use '
-        '(only applicable to non-distributed testing)')
     parser.add_argument(
         '--cfg-options',
         nargs='+',
@@ -58,16 +41,16 @@ def parse_args():
 def main():
     args = parse_args()
 
-    cfg = mmcv.Config.fromfile(args.config)
+    # register all modules in mmselfsup into the registries
+    # do not init the default scope here because it will be init in the runner
+    register_all_modules(init_default_scope=False)
+
+    # load config
+    cfg = Config.fromfile(args.config)
+    cfg.launcher = args.launcher
     if args.cfg_options is not None:
         cfg.merge_from_dict(args.cfg_options)
 
-    # set multi-process settings
-    setup_multi_processes(cfg)
-
-    # set cudnn_benchmark
-    if cfg.get('cudnn_benchmark', False):
-        torch.backends.cudnn.benchmark = True
     # work_dir is determined in this priority: CLI > segment in file > filename
     if args.work_dir is not None:
         # update configs according to CLI args if args.work_dir is not None
@@ -77,80 +60,14 @@ def main():
         work_type = args.config.split('/')[1]
         cfg.work_dir = osp.join('./work_dirs', work_type,
                                 osp.splitext(osp.basename(args.config))[0])
-    cfg.gpu_ids = [args.gpu_id]
 
-    # init distributed env first, since logger depends on the dist info.
-    if args.launcher == 'none':
-        distributed = False
-    else:
-        distributed = True
-        init_dist(args.launcher, **cfg.dist_params)
+    cfg.load_from = args.checkpoint
 
-    # create work_dir
-    mmcv.mkdir_or_exist(osp.abspath(cfg.work_dir))
+    # build the runner from config
+    runner = Runner.from_cfg(cfg)
 
-    # logger
-    timestamp = time.strftime('%Y%m%d_%H%M%S', time.localtime())
-    log_file = osp.join(cfg.work_dir, f'test_{timestamp}.log')
-    logger = get_root_logger(log_file=log_file, log_level=cfg.log_level)
-
-    # build the dataloader
-    dataset = build_dataset(cfg.data.val)
-    if 'imgs_per_gpu' in cfg.data:
-        logger.warning('"imgs_per_gpu" is deprecated. '
-                       'Please use "samples_per_gpu" instead')
-        if 'samples_per_gpu' in cfg.data:
-            logger.warning(
-                f'Got "imgs_per_gpu"={cfg.data.imgs_per_gpu} and '
-                f'"samples_per_gpu"={cfg.data.samples_per_gpu}, "imgs_per_gpu"'
-                f'={cfg.data.imgs_per_gpu} is used in this experiments')
-        else:
-            logger.warning(
-                'Automatically set "samples_per_gpu"="imgs_per_gpu"='
-                f'{cfg.data.imgs_per_gpu} in this experiments')
-        cfg.data.samples_per_gpu = cfg.data.imgs_per_gpu
-
-    # The default loader config
-    loader_cfg = dict(
-        # cfg.gpus will be ignored if distributed
-        num_gpus=len(cfg.gpu_ids),
-        dist=distributed,
-        prefetch=getattr(cfg, 'prefetch', False),
-        img_norm_cfg=cfg.img_norm_cfg)
-
-    # The overall dataloader settings
-    loader_cfg.update({
-        k: v
-        for k, v in cfg.data.items() if k not in [
-            'train', 'val', 'test', 'train_dataloader', 'val_dataloader',
-            'test_dataloader'
-        ]
-    })
-    # The specific train dataloader settings
-    test_loader_cfg = {
-        **loader_cfg,
-        'shuffle': False,  # Not shuffle by default
-        **cfg.data.get('test_dataloader', {}),
-    }
-    data_loader = build_dataloader(dataset, **test_loader_cfg)
-
-    # build the model and load checkpoint
-    model = build_algorithm(cfg.model)
-    load_checkpoint(model, args.checkpoint, map_location='cpu')
-
-    if not distributed:
-        model = MMDataParallel(model, device_ids=cfg.gpu_ids)
-        outputs = single_gpu_test(model, data_loader)
-    else:
-        model = MMDistributedDataParallel(
-            model.cuda(),
-            device_ids=[torch.cuda.current_device()],
-            broadcast_buffers=False)
-        outputs = multi_gpu_test(model, data_loader)  # dict{key: np.ndarray}
-
-    rank, _ = get_dist_info()
-    if rank == 0:
-        dataset.evaluate(outputs, logger, topk=(1, 5))
+    # start testing
+    runner.test()
 
 
 if __name__ == '__main__':
