09c721a33e2942a5841288a78074c7795bee9ab6
fangyixiao.vendor <fangyixiao@pjlab.org.cn>
2022-05-25 06:56:16 +0000

    [Refactor] refactor sampler and clean redundant codes

--- mmselfsup/datasets/__init__.py
+++ mmselfsup/datasets/__init__.py
@@ -1,6 +1,5 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .builder import DATASETS, build_dataset
-from .data_sources import *  # noqa: F401,F403
 from .dataset_wrappers import ConcatDataset, RepeatDataset
 from .pipelines import *  # noqa: F401,F403
 from .samplers import *  # noqa: F401,F403
