0edd747e60a4c5b37b19d1c44cb3c00947403c53
YuanLiuuuuuu <3463423099@qq.com>
2022-05-30 13:38:50 +0000

    [Refactor]: Refactor random aug

--- configs/benchmarks/classification/imagenet/swin-base_ft-8xb256-coslr-100e_in1k-224.py
+++ configs/benchmarks/classification/imagenet/swin-base_ft-8xb256-coslr-100e_in1k-224.py
@@ -1,4 +1,4 @@
-_base_ = 'swin-base_ft-8xb256-coslr-100e_in1k.py'
+_base_ = ['swin-base_ft-8xb256-coslr-100e_in1k.py']
 
 # model
 model = dict(
@@ -6,26 +6,58 @@ model = dict(
         img_size=224, stage_cfgs=dict(block_cfgs=dict(window_size=7))))
 
 # dataset
-img_norm_cfg = dict(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+custom_imports = dict(imports='mmcls.datasets', allow_failed_imports=False)
+preprocess_cfg = dict(
+    pixel_mean=[123.675, 116.28, 103.53],
+    pixel_std=[58.395, 57.12, 57.375],
+    to_rgb=True,
+)
+
+bgr_mean = preprocess_cfg['pixel_mean'][::-1]
+bgr_std = preprocess_cfg['pixel_std'][::-1]
+
+# train pipeline
 train_pipeline = [
+    dict(type='LoadImageFromFile'),
     dict(
-        type='RandomAug',
-        input_size=224,
-        color_jitter=0.4,
-        auto_augment='rand-m9-mstd0.5-inc1',
-        interpolation='bicubic',
-        re_prob=0.25,
-        re_mode='pixel',
-        re_count=1,
-        mean=(0.485, 0.456, 0.406),
-        std=(0.229, 0.224, 0.225))
+        type='mmcls.RandomResizedCrop',
+        scale=224,
+        backend='pillow',
+        interpolation='bicubic'),
+    dict(type='RandomFlip', prob=0.5, direction='horizontal'),
+    dict(
+        type='mmcls.RandAugment',
+        policies={{_base_.rand_increasing_policies}},
+        num_policies=2,
+        total_level=10,
+        magnitude_level=9,
+        magnitude_std=0.5,
+        hparams=dict(
+            pad_val=[round(x) for x in bgr_mean], interpolation='bicubic')),
+    dict(
+        type='mmcls.RandomErasing',
+        erase_prob=0.25,
+        mode='rand',
+        min_area_ratio=0.02,
+        max_area_ratio=1 / 3,
+        fill_color=bgr_mean,
+        fill_std=bgr_std),
+    dict(type='PackSelfSupInputs', algorithm_keys=['gt_label']),
 ]
+
+# test pipeline
 test_pipeline = [
-    dict(type='Resize', size=256, interpolation=3),
-    dict(type='CenterCrop', size=224),
-    dict(type='ToTensor'),
-    dict(type='Normalize', **img_norm_cfg)
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='mmcls.ResizeEdge',
+        scale=256,
+        edge='short',
+        backend='pillow',
+        interpolation='bicubic'),
+    dict(type='CenterCrop', crop_size=224),
+    dict(type='PackSelfSupInputs', algorithm_keys=['gt_label']),
 ]
+
 data = dict(
     samples_per_gpu=256,
     drop_last=False,
--- configs/benchmarks/classification/imagenet/vit-base-p16_ft-8xb128-coslr-100e-rpe_in1k.py
+++ configs/benchmarks/classification/imagenet/vit-base-p16_ft-8xb128-coslr-100e-rpe_in1k.py
@@ -1,4 +1,4 @@
-_base_ = 'vit-base-p16_ft-8xb128-coslr-100e_in1k.py'
+_base_ = ['vit-base-p16_ft-8xb128-coslr-100e_in1k.py']
 
 # model
 model = dict(backbone=dict(use_window=True, init_values=0.1, qkv_bias=False))
@@ -10,26 +10,58 @@ optimizer = dict(lr=8e-3)
 lr_config = dict(warmup_iters=5)
 
 # dataset
-img_norm_cfg = dict(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+custom_imports = dict(imports='mmcls.datasets', allow_failed_imports=False)
+preprocess_cfg = dict(
+    pixel_mean=[123.675, 116.28, 103.53],
+    pixel_std=[58.395, 57.12, 57.375],
+    to_rgb=True,
+)
+
+bgr_mean = preprocess_cfg['pixel_mean'][::-1]
+bgr_std = preprocess_cfg['pixel_std'][::-1]
+
+# train pipeline
 train_pipeline = [
+    dict(type='LoadImageFromFile'),
     dict(
-        type='RandomAug',
-        input_size=224,
-        color_jitter=0.4,
-        auto_augment='rand-m9-mstd0.5-inc1',
-        interpolation='bicubic',
-        re_prob=0.25,
-        re_mode='pixel',
-        re_count=1,
-        mean=(0.5, 0.5, 0.5),
-        std=(0.5, 0.5, 0.5))
+        type='mmcls.RandomResizedCrop',
+        scale=224,
+        backend='pillow',
+        interpolation='bicubic'),
+    dict(type='RandomFlip', prob=0.5, direction='horizontal'),
+    dict(
+        type='mmcls.RandAugment',
+        policies={{_base_.rand_increasing_policies}},
+        num_policies=2,
+        total_level=10,
+        magnitude_level=9,
+        magnitude_std=0.5,
+        hparams=dict(
+            pad_val=[round(x) for x in bgr_mean], interpolation='bicubic')),
+    dict(
+        type='mmcls.RandomErasing',
+        erase_prob=0.25,
+        mode='rand',
+        min_area_ratio=0.02,
+        max_area_ratio=1 / 3,
+        fill_color=bgr_mean,
+        fill_std=bgr_std),
+    dict(type='PackSelfSupInputs', algorithm_keys=['gt_label']),
 ]
+
+# test pipeline
 test_pipeline = [
-    dict(type='Resize', size=256, interpolation=3),
-    dict(type='CenterCrop', size=224),
-    dict(type='ToTensor'),
-    dict(type='Normalize', **img_norm_cfg)
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='mmcls.ResizeEdge',
+        scale=256,
+        edge='short',
+        backend='pillow',
+        interpolation='bicubic'),
+    dict(type='CenterCrop', crop_size=224),
+    dict(type='PackSelfSupInputs', algorithm_keys=['gt_label']),
 ]
+
 data = dict(
     train=dict(pipeline=train_pipeline),
     val=dict(pipeline=test_pipeline),
