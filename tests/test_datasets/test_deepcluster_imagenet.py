fa589f82ec121604465a47b76fb7644dca2bbdf6
fangyixiao.vendor <fangyixiao@pjlab.org.cn>
2022-06-06 09:29:01 +0000

    [Refactor] add unit test for deepcluster dataset and sampler

---
+++ tests/test_datasets/test_deepcluster_imagenet.py
@@ -0,0 +1,43 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+import os.path as osp
+
+import pytest
+
+from mmselfsup.datasets import DeepClusterImageNet
+from mmselfsup.utils import register_all_modules
+
+# dataset settings
+train_pipeline = [
+    dict(type='LoadImageFromFile', file_client_args=dict(backend='disk')),
+    dict(type='RandomResizedCrop', size=4)
+]
+
+
+def test_deepcluster_dataset():
+    register_all_modules()
+
+    data = dict(
+        ann_file=osp.join(
+            osp.dirname(__file__), '..', 'data', 'data_list.txt'),
+        metainfo=None,
+        data_root=osp.join(osp.dirname(__file__), '..', 'data'),
+        data_prefix=None,
+        pipeline=train_pipeline)
+    dataset = DeepClusterImageNet(**data)
+    assert len(dataset.clustering_labels) == 2
+
+    x = dataset[0]
+    print(x)
+    assert x['img'].shape == (4, 4, 3)
+    assert x['clustering_label'] == -1
+    assert x['sample_idx'] == 0
+
+    with pytest.raises(AssertionError):
+        dataset.assign_labels([1])
+
+    dataset.assign_labels([1, 0])
+    assert dataset.clustering_labels[0] == 1
+    assert dataset.clustering_labels[1] == 0
+
+    x = dataset[0]
+    assert x['clustering_label'] == 1
---
+++ tests/test_datasets/test_samplers/test_deepcluster_samplers.py
@@ -0,0 +1,53 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from unittest import TestCase
+from unittest.mock import patch
+
+from mmengine.data.sampler import DefaultSampler
+
+from mmselfsup.datasets.samplers import DeepClusterSampler
+
+
+class TestDeepClusterSampler(TestCase):
+
+    def setUp(self):
+        self.data_length = 100
+        self.dataset = list(range(self.data_length))
+
+    @patch('mmengine.data.sampler.get_dist_info', return_value=(0, 1))
+    def test_deepcluster_sampler(self, mock):
+        # test round_up=True
+        sampler = DeepClusterSampler(
+            self.dataset, round_up=True, shuffle=False)
+        self.assertEqual(sampler.total_size, self.data_length)
+        self.assertEqual(sampler.num_samples, self.data_length)
+        self.assertEqual(list(sampler), list(range(self.data_length)))
+
+        # test round_up=False
+        sampler = DeepClusterSampler(
+            self.dataset, round_up=False, shuffle=False)
+        self.assertEqual(sampler.total_size, self.data_length)
+        self.assertEqual(sampler.num_samples, self.data_length)
+        self.assertEqual(list(sampler), list(range(self.data_length)))
+
+        # test the consistency
+        default_sampler = DefaultSampler(self.dataset, seed=0)
+        sampler = DeepClusterSampler(self.dataset, seed=0, replace=False)
+        self.assertEqual(list(sampler), list(default_sampler))
+
+        # test replace
+        default_sampler = DefaultSampler(self.dataset, seed=0)
+        sampler = DeepClusterSampler(self.dataset, seed=0, replace=True)
+        self.assertEqual(len(list(sampler)), self.data_length)
+        self.assertNotEqual(list(sampler), list(default_sampler))
+
+    @patch('mmengine.data.sampler.get_dist_info', return_value=(0, 1))
+    def test_set_uniform_indices(self, mock):
+        sampler = DeepClusterSampler(self.dataset, seed=0, replace=True)
+        idx_0 = list(sampler)
+
+        labels = [0] * 50 + [1] * 50
+        sampler.set_uniform_indices(labels, 2)
+        idx_1 = list(sampler)
+
+        self.assertEqual(len(idx_0), len(idx_1))
+        self.assertNotEqual(idx_0, idx_1)
