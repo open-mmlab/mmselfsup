962f9b975204e8af85a6b8a24943c0699698ea79
fangyixiao.vendor <fangyixiao@pjlab.org.cn>
2022-06-09 04:18:11 +0000

    [Refactor] refactor multi_cls_head and clean cls algorithm

--- mmselfsup/models/algorithms/__init__.py
+++ mmselfsup/models/algorithms/__init__.py
@@ -3,11 +3,9 @@ from .barlowtwins import BarlowTwins
 from .base import BaseModel
 from .byol import BYOL
 from .cae import CAE
-from .classification import Classification
 from .deepcluster import DeepCluster
 from .densecl import DenseCL
 from .mae import MAE
-from .mmcls_classifier_wrapper import MMClsImageClassifierWrapper
 from .moco import MoCo
 from .mocov3 import MoCoV3
 from .npid import NPID
@@ -20,8 +18,7 @@ from .simsiam import SimSiam
 from .swav import SwAV
 
 __all__ = [
-    'BaseModel', 'BarlowTwins', 'BYOL', 'Classification', 'DeepCluster',
-    'DenseCL', 'MoCo', 'NPID', 'ODC', 'RelativeLoc', 'RotationPred', 'SimCLR',
-    'SimSiam', 'SwAV', 'MAE', 'MoCoV3', 'SimMIM',
-    'MMClsImageClassifierWrapper', 'CAE'
+    'BaseModel', 'BarlowTwins', 'BYOL', 'DeepCluster', 'DenseCL', 'MoCo',
+    'NPID', 'ODC', 'RelativeLoc', 'RotationPred', 'SimCLR', 'SimSiam', 'SwAV',
+    'MAE', 'MoCoV3', 'SimMIM', 'CAE'
 ]
