408b9fb18720863c1e637e842fffa2a086b92920
YuanLiuuuuuu <3463423099@qq.com>
2022-06-20 06:53:35 +0000

    [Fix]: Fix layer wise learning rate decay optim wrapper

--- mmselfsup/registry/__init__.py
+++ mmselfsup/registry/__init__.py
@@ -1,8 +1,8 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .registry import (DATASETS, HOOKS, MODELS, OPTIM_WRAPPER_CONSTRUCTORS,
-                       OPTIMIZERS, TRANSFORMS)
+                       OPTIM_WRAPPERS, OPTIMIZERS, TRANSFORMS)
 
 __all__ = [
     'MODELS', 'DATASETS', 'TRANSFORMS', 'HOOKS', 'OPTIMIZERS',
-    'OPTIM_WRAPPER_CONSTRUCTORS'
+    'OPTIM_WRAPPER_CONSTRUCTORS', 'OPTIM_WRAPPERS'
 ]
