8910743c6e8b7898e6e3c76a40da2925f3ac5f59
YuanLiuuuuuu <3463423099@qq.com>
2022-07-06 08:18:46 +0000

    [Fix]: Fix multi-head config

--- configs/benchmarks/classification/imagenet/resnet50_mhead_linear-8xb32-steplr-90e_in1k.py
+++ configs/benchmarks/classification/imagenet/resnet50_mhead_linear-8xb32-steplr-90e_in1k.py
@@ -4,53 +4,77 @@ _base_ = [
     '../_base_/schedules/sgd_steplr-100e.py',
     '../_base_/default_runtime.py',
 ]
-# Multi-head linear evaluation setting
 
-model = dict(backbone=dict(frozen_stages=4))
+# lighting params, in order of BGR
+EIGVAL = [55.4625, 4.7940, 1.1475]
+EIGVEC = [
+    [-0.5836, -0.6948, 0.4203],
+    [-0.5808, -0.0045, -0.8140],
+    [-0.5675, 0.7192, 0.4009],
+]
 
-# dataset settings
-img_norm_cfg = dict(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+# dataset
 train_pipeline = [
-    dict(type='RandomResizedCrop', size=224),
-    dict(type='RandomHorizontalFlip'),
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='RandomResizedCrop',
+        scale=224,
+        backend='pillow',
+        interpolation='bicubic'),
+    dict(type='RandomFlip', prob=0.5, direction='horizontal'),
     dict(
         type='ColorJitter',
         brightness=0.4,
         contrast=0.4,
         saturation=0.4,
         hue=0.),
-    dict(type='ToTensor'),
-    dict(type='Lighting'),
-    dict(type='Normalize', **img_norm_cfg),
+    dict(
+        type='Lighting',
+        eigval=EIGVAL,
+        eigvec=EIGVEC,
+    ),
+    dict(type='PackClsInputs')
 ]
 test_pipeline = [
-    dict(type='Resize', size=256),
-    dict(type='CenterCrop', size=224),
-    dict(type='ToTensor'),
-    dict(type='Normalize', **img_norm_cfg),
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='ResizeEdge',
+        scale=256,
+        edge='short',
+        backend='pillow',
+        interpolation='bicubic'),
+    dict(type='CenterCrop', crop_size=224),
+    dict(type='PackClsInputs')
 ]
-data = dict(
-    train=dict(pipeline=train_pipeline), val=dict(pipeline=test_pipeline))
+train_dataloader = dict(dataset=dict(pipeline=train_pipeline))
+val_dataloader = dict(dataset=dict(pipeline=test_pipeline))
+
+# MoCo v1/v2 linear evaluation setting
+model = dict(
+    backbone=dict(out_indices=[0, 1, 2, 3], frozen_stages=4),
+    head=dict(in_indices=[1, 2, 3, 4]))
 
 # optimizer
 optimizer = dict(
-    type='SGD',
-    lr=0.01,
-    momentum=0.9,
-    weight_decay=1e-4,
-    paramwise_options=dict(norm_decay_mult=0.),
-    nesterov=True)
-
-# learning rate scheduler
-param_scheduler = [
-    dict(
-        type='MultiStepLR', by_epoch=True, milestones=[30, 60, 90], gamma=0.1)
-]
+    type='SGD', lr=0.01, momentum=0.9, weight_decay=1e-4, nesterov=True)
+optim_wrapper = dict(
+    type='OptimWrapper',
+    optimizer=optimizer,
+    paramwise_cfg=dict(norm_decay_mult=0.0))
 
 # runtime settings
-runner = dict(type='EpochBasedRunner', max_epochs=90)
+default_hooks = dict(
+    checkpoint=dict(type='CheckpointHook', interval=10, max_keep_ckpts=3))
 
-# the max_keep_ckpts controls the max number of ckpt file in your work_dirs
-# if it is 3, when CheckpointHook (in mmcv) saves the 4th ckpt
-# it will remove the oldest one to keep the number of total ckpts as 3
-checkpoint_config = dict(interval=10, max_keep_ckpts=3)
+# evaluator
+val_evaluator = dict(
+    _delete_=True,
+    type='MultiHeadEvaluator',
+    metrics=dict(
+        head1=dict(type='mmcls.Accuracy', topk=(1, 5)),
+        head2=dict(type='mmcls.Accuracy', topk=(1, 5)),
+        head3=dict(type='mmcls.Accuracy', topk=(1, 5)),
+        head4=dict(type='mmcls.Accuracy', topk=(1, 5))))
+
+# epochs
+train_cfg = dict(type='EpochBasedTrainLoop', max_epochs=90)
