1bfb151a8914de17e194617d8e0f51be2d579d61
YuanLiuuuuuu <3463423099@qq.com>
2022-07-12 15:41:46 +0800

    [Fix]: Add comment to config

--- configs/benchmarks/classification/imagenet/vit-base-p16_ft-8xb128-coslr-100e_in1k.py
+++ configs/benchmarks/classification/imagenet/vit-base-p16_ft-8xb128-coslr-100e_in1k.py
@@ -1,3 +1,4 @@
+# mmcls:: means we use the default settings from MMClassification
 _base_ = [
     'mmcls::_base_/datasets/imagenet_bs64_swin_224.py',
     'mmcls::_base_/schedules/imagenet_bs1024_adamw_swin.py',
