d1cf2d8d2308d7a81db5c2071cc2124755a5a6d6
YuanLiuuuuuu <3463423099@qq.com>
2022-07-16 15:56:22 +0800

    [Refactor]: Refactor mae model config

--- configs/selfsup/_base_/models/mae_vit-base-p16.py
+++ configs/selfsup/_base_/models/mae_vit-base-p16.py
@@ -2,7 +2,9 @@
 model = dict(
     type='MAE',
     data_preprocessor=dict(
-        mean=[124, 117, 104], std=[59, 58, 58], bgr_to_rgb=True),
+        mean=[123.675, 116.28, 103.53],
+        std=[58.395, 57.12, 57.375],
+        bgr_to_rgb=True),
     backbone=dict(type='MAEViT', arch='b', patch_size=16, mask_ratio=0.75),
     neck=dict(
         type='MAEPretrainDecoder',
@@ -18,4 +20,8 @@ model = dict(
         type='MAEPretrainHead',
         norm_pix=True,
         patch_size=16,
-        loss=dict(type='MAEReconstructionLoss')))
+        loss=dict(type='MAEReconstructionLoss')),
+    init_cfg=[
+        dict(type='Xavier', distribution='uniform', layer='Linear'),
+        dict(type='Constant', layer='LayerNorm', val=1.0, bias=0.0)
+    ])
