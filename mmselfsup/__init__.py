669a7fc95873895b6eb2d930b41ea3814a995a8a
fangyixiao18 <fangyx18@hotmail.com>
2022-07-18 10:29:28 +0800

    remove mmengine

--- mmselfsup/__init__.py
+++ mmselfsup/__init__.py
@@ -1,7 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import mmcls
 import mmcv
-import mmengine
 from mmengine.utils import digit_version
 
 from .version import __version__
@@ -9,10 +8,6 @@ from .version import __version__
 mmcv_minimum_version = '2.0.0rc0'
 mmcv_maximum_version = '2.1.0'
 mmcv_version = digit_version(mmcv.__version__)
-
-mmengine_minimum_version = '0.0.0'
-mmengine_maximum_version = '0.2.0'
-mmengine_version = digit_version(mmengine.__version__)
 
 mmcls_minimum_version = '1.0.0rc0'
 mmcls_version = digit_version(mmcls.__version__)
@@ -22,12 +17,6 @@ assert (mmcv_version >= digit_version(mmcv_minimum_version)
         and mmcv_version <= digit_version(mmcv_maximum_version)), \
     f'MMCV=={mmcv.__version__} is used but incompatible. ' \
     f'Please install mmcv>={mmcv_minimum_version}, <={mmcv_maximum_version}.'
-
-assert (mmengine_version >= digit_version(mmengine_minimum_version)
-        and mmengine_version <= digit_version(mmengine_maximum_version)), \
-    f'MMEngine=={mmengine.__version__} is used but incompatible. ' \
-    f'Please install mmengine>={mmengine_minimum_version}, ' \
-    f'<={mmengine_maximum_version}.'
 
 assert mmcls_version >= digit_version(mmcls_minimum_version), \
     f'MMClassification=={mmcls.__version__} is used but incompatible. ' \
