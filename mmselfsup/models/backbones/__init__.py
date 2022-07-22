f7927cff7e160ad9da7bcd491ddd5f5450675c76
YuanLiuuuuuu <3463423099@qq.com>
2022-07-22 13:58:55 +0800

    [Fix]: Remove mim-vit from __init__.py

--- mmselfsup/models/backbones/__init__.py
+++ mmselfsup/models/backbones/__init__.py
@@ -1,13 +1,12 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .cae_vit import CAEViT
 from .mae_vit import MAEViT
-from .mim_cls_vit import MIMVisionTransformer
 from .mocov3_vit import MoCoV3ViT
 from .resnet import ResNet, ResNetSobel, ResNetV1d
 from .resnext import ResNeXt
 from .simmim_swin import SimMIMSwinTransformer
 
 __all__ = [
-    'ResNet', 'ResNetSobel', 'ResNetV1d', 'ResNeXt', 'MAEViT',
-    'MIMVisionTransformer', 'MoCoV3ViT', 'SimMIMSwinTransformer', 'CAEViT'
+    'ResNet', 'ResNetSobel', 'ResNetV1d', 'ResNeXt', 'MAEViT', 'MoCoV3ViT',
+    'SimMIMSwinTransformer', 'CAEViT'
 ]
