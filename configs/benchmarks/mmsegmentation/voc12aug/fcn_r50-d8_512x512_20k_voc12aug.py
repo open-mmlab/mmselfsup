0cd707833c034ff2d90196b62e893fe2ceed6c4a
wangbo-zhao <wangbo.zhao96@gmail.com>
2022-08-05 10:26:13 +0800

    modify config files

--- configs/benchmarks/mmsegmentation/voc12aug/fcn_r50-d8_512x512_20k_voc12aug.py
+++ configs/benchmarks/mmsegmentation/voc12aug/fcn_r50-d8_512x512_20k_voc12aug.py
@@ -1,5 +1,5 @@
 _base_ = [
-    '../_base_/models/fcn_r50-d8.py', '../_base_/datasets/pascal_voc12.py',
+    '../_base_/models/fcn_r50-d8.py', '../_base_/datasets/pascal_voc12_aug.py',
     '../_base_/default_runtime.py', '../_base_/schedules/schedule_20k.py'
 ]
 
