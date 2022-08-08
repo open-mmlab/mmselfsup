9afb3165216744a18cf03613464f39aaf0bffae5
fangyixiao18 <fangyx18@hotmail.com>
2022-08-08 14:25:58 +0800

    update typehint

--- mmselfsup/utils/misc.py
+++ mmselfsup/utils/misc.py
@@ -1,12 +1,13 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+import torch.nn as nn
 from mmengine.model import BaseModel, is_model_wrapper
 
 
-def get_model(model) -> BaseModel:
+def get_model(model: nn.Module) -> BaseModel:
     """Get model if the input model is a model wrapper.
 
     Args:
-        model (BaseModel): A model may be a model wrapper.
+        model (nn.Module): A model may be a model wrapper.
 
     Returns:
         BaseModel: The model without model wrapper.
