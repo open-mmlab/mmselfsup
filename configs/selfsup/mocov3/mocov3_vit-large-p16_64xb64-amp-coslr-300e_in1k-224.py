80263eed0c8a1e517c118c5a060cde45c43fad35
fangyixiao18 <fangyx18@hotmail.com>
2022-08-18 14:31:33 +0800

    update mocov3 vit-l config

--- configs/selfsup/mocov3/mocov3_vit-large-p16_64xb64-amp-coslr-300e_in1k-224.py
+++ configs/selfsup/mocov3/mocov3_vit-large-p16_64xb64-amp-coslr-300e_in1k-224.py
@@ -1,5 +1,7 @@
 _base_ = 'mocov3_vit-base-p16_16xb256-fp16-coslr-300e_in1k-224.py'
 
+train_dataloader = dict(batch_size=64, num_workers=8)
+
 # model settings
 model = dict(
     backbone=dict(arch='large'),  # embed_dim = 768
