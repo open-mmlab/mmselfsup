2043b923651ac8e39019f6a9bbb8381474c1b76b
fangyixiao18 <fangyx18@hotmail.com>
2022-08-22 21:10:47 +0800

    update config

--- configs/benchmarks/classification/imagenet/vit-base-p16_ft-8xb64-coslr-150e_in1k.py
+++ configs/benchmarks/classification/imagenet/vit-base-p16_ft-8xb64-coslr-150e_in1k.py
@@ -1,6 +1,6 @@
 _base_ = [
     '../_base_/models/vit-base-p16.py',
-    '../_base_/datasets/imagenet_mocov3_vit.py',
+    '../_base_/datasets/imagenet_mocov3_ft.py',
     '../_base_/schedules/adamw_coslr-100e_in1k.py',
     '../_base_/default_runtime.py',
 ]
---
+++ configs/benchmarks/classification/imagenet/vit-large-p16_linear-8xb128-coslr-90e_in1k.py
@@ -0,0 +1,8 @@
+_base_ = 'vit-small-p16_linear-8xb128-coslr-90e_in1k.py'
+
+# MoCo v3 linear probing setting
+model = dict(
+    backbone=dict(arch='large', frozen_stages=12, norm_eval=True),
+    head=dict(in_channels=1024))
+
+optim_wrapper = dict(clip_grad=dict(max_norm=5.0, error_if_nonfinite=False))
