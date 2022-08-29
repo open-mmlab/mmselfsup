9e97eefe1c73a9eca7f63e1aef45c43fb1cf8113
daidaiershidi <1154864382@qq.com>
2022-08-29 14:14:50 +0800

    [Feature]: Add Backbone of MaskFeat

--- mmselfsup/models/backbones/__init__.py
+++ mmselfsup/models/backbones/__init__.py
@@ -1,6 +1,7 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .cae_vit import CAEViT
 from .mae_vit import MAEViT
+from .maskfeat_vit import MaskFeatViT
 from .mim_cls_vit import MIMVisionTransformer
 from .resnet import ResNet, ResNetV1d
 from .resnext import ResNeXt
@@ -8,6 +9,7 @@ from .simmim_swin import SimMIMSwinTransformer
 from .vision_transformer import VisionTransformer
 
 __all__ = [
-    'ResNet', 'ResNetV1d', 'ResNeXt', 'MAEViT', 'MIMVisionTransformer',
-    'VisionTransformer', 'SimMIMSwinTransformer', 'CAEViT'
+    'ResNet', 'ResNetV1d', 'ResNeXt', 'MAEViT', 'MaskFeatViT',
+    'MIMVisionTransformer', 'VisionTransformer', 'SimMIMSwinTransformer',
+    'CAEViT'
 ]
