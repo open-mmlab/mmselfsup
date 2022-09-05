42379210c1bcda5ae2fe1fc454e48fb7d7abb2c0
YuanLiuuuuuu <3463423099@qq.com>
2022-09-05 19:59:37 +0800

    [Feature]: Add ceph to vit-h-448

--- configs/benchmarks/classification/imagenet/vit-huge-p16_ft-32xb8-coslr-50e_in1k-448.py
+++ configs/benchmarks/classification/imagenet/vit-huge-p16_ft-32xb8-coslr-50e_in1k-448.py
@@ -8,7 +8,15 @@ model = dict(
     head=dict(in_channels=1280))
 
 # dataset settings
-file_client_args = dict(backend='disk')
+# file_client_args = dict(backend='disk')
+file_client_args = dict(
+    backend='petrel',
+    path_mapping=dict({
+        './data/imagenet':
+        's3://openmmlab/datasets/classification/imagenet',
+        'data/imagenet':
+        's3://openmmlab/datasets/classification/imagenet'
+    }))
 
 train_pipeline = [
     dict(type='LoadImageFromFile', file_client_args=file_client_args),
