44bb30f4b8fa53c6a1c4e28df5573c98c699519f
YuanLiuuuuuu <3463423099@qq.com>
2022-09-08 14:41:41 +0800

    [Fix]: Refine mim_slurm_test.sh

--- tools/benchmarks/classification/mim_slurm_test.sh
+++ tools/benchmarks/classification/mim_slurm_test.sh
@@ -4,7 +4,7 @@ set -x
 
 PARTITION=$1
 CFG=$2  # use cfgs under "configs/benchmarks/classification/imagenet/*.py"
-CHECKPOINT=$3
+CHECKPOINT=$3  # pretrained model
 GPUS=${GPUS:-8}
 GPUS_PER_NODE=${GPUS_PER_NODE:-8}
 CPUS_PER_TASK=${CPUS_PER_TASK:-5}
@@ -12,12 +12,11 @@ SRUN_ARGS=${SRUN_ARGS:-""}
 PY_ARGS=${@:4}
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
-mim test mmcls \
-    $CFG \
+mim test mmcls $CFG \
     --checkpoint $CHECKPOINT \
     --launcher slurm -G $GPUS \
     --gpus-per-node $GPUS_PER_NODE \
     --cpus-per-task $CPUS_PER_TASK \
     --partition $PARTITION \
     --srun-args "$SRUN_ARGS" \
-    $PY_ARGS
+    --cfg-options $PY_ARGS \
