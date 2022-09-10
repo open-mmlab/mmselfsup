70f833959dcf9e3717e4a6d6b629cf130fedf552
daidaiershidi <1154864382@qq.com>
2022-09-10 21:26:26 +0800

    [Feature]: Add MaskfeatMaskGenerator Pipeline

--- mmselfsup/datasets/pipelines/__init__.py
+++ mmselfsup/datasets/pipelines/__init__.py
@@ -1,9 +1,10 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .transforms import (BEiTMaskGenerator, GaussianBlur, Lighting,
-                         RandomAppliedTrans, RandomAug, SimMIMMaskGenerator,
-                         Solarization, ToTensor)
+                         MaskfeatMaskGenerator, RandomAppliedTrans, RandomAug,
+                         SimMIMMaskGenerator, Solarization, ToTensor)
 
 __all__ = [
     'GaussianBlur', 'Lighting', 'RandomAppliedTrans', 'Solarization',
-    'RandomAug', 'SimMIMMaskGenerator', 'ToTensor', 'BEiTMaskGenerator'
+    'RandomAug', 'SimMIMMaskGenerator', 'ToTensor', 'BEiTMaskGenerator',
+    'MaskfeatMaskGenerator'
 ]
