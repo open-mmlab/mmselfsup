fa11a780e503ff9cb8b8082caa76e98909ecbb71
daidaiershidi <1154864382@qq.com>
2022-09-22 09:10:05 +0800

    [fix] add new paramwise_options in fine-tuning config

--- configs/benchmarks/classification/imagenet/vit-base-p16_ft-8xb256-coslr-100e_in1k.py
+++ configs/benchmarks/classification/imagenet/vit-base-p16_ft-8xb256-coslr-100e_in1k.py
@@ -51,6 +51,8 @@ optimizer = dict(
     paramwise_options={
         'ln': dict(weight_decay=0.),
         'bias': dict(weight_decay=0.),
+        'pos_embed': dict(weight_decay=0.),
+        'cls_token': dict(weight_decay=0.),
     },
     constructor='TransformerFinetuneConstructor',
     model_type='vit',
