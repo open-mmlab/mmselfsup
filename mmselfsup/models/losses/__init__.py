f8b4a77ff4a386df8169f4de1759c181e0e21972
daidaiershidi <1154864382@qq.com>
2022-09-26 15:05:49 +0800

    [Feature]: Add Loss of MaskFeat

--- mmselfsup/models/losses/__init__.py
+++ mmselfsup/models/losses/__init__.py
@@ -3,6 +3,7 @@ from .cae_loss import CAELoss
 from .cosine_similarity_loss import CosineSimilarityLoss
 from .cross_correlation_loss import CrossCorrelationLoss
 from .mae_loss import MAEReconstructionLoss
+from .maskfeat_loss import MaskFeatReconstructionLoss
 from .reconstruction_loss import PixelReconstructionLoss
 from .simmim_loss import SimMIMReconstructionLoss
 from .swav_loss import SwAVLoss
@@ -10,5 +11,5 @@ from .swav_loss import SwAVLoss
 __all__ = [
     'CAELoss', 'CrossCorrelationLoss', 'CosineSimilarityLoss',
     'MAEReconstructionLoss', 'SimMIMReconstructionLoss', 'SwAVLoss',
-    'PixelReconstructionLoss'
+    'PixelReconstructionLoss', 'MaskFeatReconstructionLoss'
 ]
