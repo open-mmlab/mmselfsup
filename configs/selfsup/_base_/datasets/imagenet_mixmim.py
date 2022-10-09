fbfa5eabb90e1235a829f74d3c02e4ffab995765
wangbo-zhao <wangbo.zhao96@gmail.com>
2022-10-09 06:27:18 +0000

    add mixmim

--- configs/selfsup/_base_/datasets/imagenet_mixmim.py
+++ configs/selfsup/_base_/datasets/imagenet_mixmim.py
@@ -1,9 +1,18 @@
 # dataset settings
 custom_imports = dict(imports='mmcls.datasets', allow_failed_imports=False)
+
+file_client_args = dict(
+    backend='petrel',
+    path_mapping=dict({
+        '.data/imagenet/': 'openmmlab:s3://openmmlab/datasets/classification/imagenet/',
+        'data/imagenet/': 'openmmlab:s3://openmmlab/datasets/classification/imagenet/'
+    }))
+
+
 dataset_type = 'mmcls.ImageNet'
-# data_root = 'data/imagenet/'
-data_root = '/data/common/ImageNet/'
-file_client_args = dict(backend='disk')
+data_root = 'data/imagenet/'
+# data_root = '/data/common/ImageNet/'
+# file_client_args = dict(backend='disk')
 
 train_pipeline = [  # √
     dict(type='LoadImageFromFile', file_client_args=file_client_args),
@@ -26,6 +35,7 @@ train_dataloader = dict(
     dataset=dict(
         type=dataset_type,
         data_root=data_root,
-        ann_file="/home/nus-zwb/research/data/imagenet/train.txt",
+        # ann_file="/home/nus-zwb/research/data/imagenet/train.txt",
+        ann_file="/mnt/lustre/zhaowangbo/research/2022ICLR/data/imagenet/meta/train.txt",
         data_prefix=dict(img_path='train/'),
         pipeline=train_pipeline))
