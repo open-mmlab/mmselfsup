0dc46e0f12ab0747ba308aa2022a173f3c386fd5
wangbo-zhao <wangbo.zhao96@gmail.com>
2022-10-10 02:34:59 +0000

    add mixmim

--- configs/selfsup/_base_/datasets/imagenet_mixmim.py
+++ configs/selfsup/_base_/datasets/imagenet_mixmim.py
@@ -30,6 +30,7 @@ train_dataloader = dict(
     batch_size=128,
     num_workers=8,
     persistent_workers=True,
+    pin_memory=True,
     sampler=dict(type='DefaultSampler', shuffle=True),
     collate_fn=dict(type='default_collate'),
     dataset=dict(
