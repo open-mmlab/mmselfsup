bfd22d7a4dcadc955dc0acb444025f17477d1fb9
fangyixiao18 <fangyx18@hotmail.com>
2022-11-11 16:51:16 +0800

    add dalle target generator

--- mmselfsup/models/target_generators/__init__.py
+++ mmselfsup/models/target_generators/__init__.py
@@ -1,5 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+from .dall_e import Encoder
 from .hog_generator import HOGGenerator
 from .vqkd import VQKD
 
-__all__ = ['HOGGenerator', 'VQKD']
+__all__ = ['HOGGenerator', 'VQKD', 'Encoder']
