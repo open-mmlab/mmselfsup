fc35c43afd557ba3abccc084ad8f0a4558060f45
wangbo-zhao <wangbo.zhao96@gmail.com>
2022-12-12 02:43:05 +0000

    [Feature] Support MixMIM Pretrain and Finetuning

--- mmselfsup/models/backbones/__init__.py
+++ mmselfsup/models/backbones/__init__.py
@@ -5,7 +5,6 @@ from .mae_vit import MAEViT
 from .maskfeat_vit import MaskFeatViT
 from .milan_vit import MILANViT
 from .mixmim_backbone import MixMIMTransformer, MixMIMTransformerPretrain
-from .mixmim_backbone import MixMIMTransformer, MixMIMTransformerPretrain
 from .mocov3_vit import MoCoV3ViT
 from .resnet import ResNet, ResNetSobel, ResNetV1d
 from .resnext import ResNeXt
@@ -13,6 +12,6 @@ from .simmim_swin import SimMIMSwinTransformer
 
 __all__ = [
     'ResNet', 'ResNetSobel', 'ResNetV1d', 'ResNeXt', 'MAEViT', 'MoCoV3ViT',
-    'SimMIMSwinTransformer', 'CAEViT', 'MaskFeatViT', 'BEiTViT', 'MILANViT',
-    'MixMIMTransformerPretrain', 'MixMIMTransformer'
+    'SimMIMSwinTransformer', 'CAEViT', 'MixMIMTransformerPretrain',
+    'MixMIMTransformer'
 ]
