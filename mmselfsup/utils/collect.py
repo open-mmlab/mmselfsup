d1096c0f12b52699cfbd3d1c7983d77c249bfdb1
fangyixiao18 <fangyx18@hotmail.com>
2022-12-15 13:09:51 +0800

    fix bugs of knn

--- mmselfsup/utils/collect.py
+++ mmselfsup/utils/collect.py
@@ -1,93 +1,87 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from typing import Dict, Optional
-
 import mmengine
-import numpy as np
 import torch
+from mmengine.dist import collect_results_gpu, get_rank
 from torch.utils.data import DataLoader
-
-from .gather import gather_tensors_batch
 
 
 def nondist_forward_collect(func: object, data_loader: DataLoader,
-                            length: int) -> Dict:
+                            length: int) -> dict:
     """Forward and collect network outputs.
 
     This function performs forward propagation and collects outputs.
     It can be used to collect results, features, losses, etc.
 
     Args:
-        func (function): The function to process data. The output must be
-            a dictionary of CPU tensors.
+        func (function): The function to process data.
         data_loader (DataLoader): the torch DataLoader to yield data.
         length (int): Expected length of output arrays.
 
     Returns:
-        results_all (Dict(np.ndarray)): The concatenated outputs.
+        Dict[str, torch.Tensor]: The concatenated outputs.
     """
     results = []
     prog_bar = mmengine.ProgressBar(len(data_loader))
     for _, data in enumerate(data_loader):
         with torch.no_grad():
-            result = func(data)  # output: feat_dict
-        results.append(result)  # list of feat_dict
+            result = func(data)  # dict{key: tensor}
+        results.append(result)
         prog_bar.update()
 
-    results_all = {}
+    results_dict = {}
     for k in results[0].keys():
-        results_all[k] = np.concatenate(
-            [batch[k].numpy() for batch in results], axis=0)
-        assert results_all[k].shape[0] == length
-    return results_all
+        results_dict[k] = torch.cat([batch[k] for batch in results], dim=0)
+        assert results_dict[k].size(0) == length
+    return results_dict
 
 
-def dist_forward_collect(func: object,
-                         data_loader: DataLoader,
-                         rank: int,
-                         length: int,
-                         ret_rank: Optional[int] = -1) -> Dict:
+def dist_forward_collect(func: object, data_loader: DataLoader,
+                         length: int) -> dict:
     """Forward and collect network outputs in a distributed manner.
 
     This function performs forward propagation and collects outputs.
     It can be used to collect results, features, losses, etc.
 
     Args:
-        func (function): The function to process data. The output must be
-            a dictionary of CPU tensors.
+        func (function): The function to process data.
         data_loader (DataLoader): the torch DataLoader to yield data.
-        rank (int): This process id.
         length (int): Expected length of output arrays.
-        ret_rank (int): The process that returns.
-            Other processes will return None.
 
     Returns:
-        results_all (dict(np.ndarray)): The concatenated outputs.
+        Dict[str, torch.Tensor]: The collected outputs.
     """
+    rank = get_rank()
     results = []
     if rank == 0:
         prog_bar = mmengine.ProgressBar(len(data_loader))
     for _, data in enumerate(data_loader):
         with torch.no_grad():
             result = func(data)  # dict{key: tensor}
+
+        # batch_dict = {}
+        # for k in result.keys():
+        #     batch_local = []
+        #     batch_local.extend(result[k].tolist())
+        #     batch_gathered = collect_results_gpu(
+        #         batch_local,
+        #         len(batch_local) * get_world_size())
+
+        #     if rank == 0:
+        #         batch_dict[k] = torch.Tensor(
+        #             batch_gathered, device=torch.device('cuda:0'))
+
         results.append(result)
 
         if rank == 0:
             prog_bar.update()
 
-    results_all = {}
+    results_dict = {}
     for k in results[0].keys():
-        results_cat = np.concatenate([batch[k].numpy() for batch in results],
-                                     axis=0)
-        if ret_rank == -1:
-            results_gathered = gather_tensors_batch(results_cat, part_size=20)
-            results_strip = np.concatenate(results_gathered, axis=0)[:length]
-        else:
-            results_gathered = gather_tensors_batch(
-                results_cat, part_size=20, ret_rank=ret_rank)
-            if rank == ret_rank:
-                results_strip = np.concatenate(
-                    results_gathered, axis=0)[:length]
-            else:
-                results_strip = None
-        results_all[k] = results_strip
-    return results_all
+        results_local = []
+        for batch in results:
+            results_local.extend(batch[k].tolist())
+        results_gathered = collect_results_gpu(results_local, length)
+        if rank == 0:
+            results_dict[k] = torch.Tensor(results_gathered).to(
+                torch.device('cuda:0'))
+    return results_dict
