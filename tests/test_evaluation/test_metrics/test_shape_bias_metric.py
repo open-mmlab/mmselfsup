b918fc08fbb0eb3b9f7543fc464e6002d2e63de8
YuanLiuuuuuu <3463423099@qq.com>
2022-12-19 14:34:19 +0800

    [Fix]: Fix lint

--- tests/test_evaluation/test_metrics/test_shape_bias_metric.py
+++ tests/test_evaluation/test_metrics/test_shape_bias_metric.py
@@ -1,11 +1,13 @@
-from mmselfsup.evaluation import ShapeBiasMetric
+# Copyright (c) OpenMMLab. All rights reserved.
 import torch
+
+from mmselfsup.evaluation import ShapeBiasMetric
 
 
 def test_shape_bias_metric():
     data_sample = dict()
     data_sample['pred_label'] = dict(
-        score=torch.rand(1000,), label=torch.tensor(1))
+        score=torch.rand(1000, ), label=torch.tensor(1))
     data_sample['gt_label'] = dict(label=torch.tensor(1))
     data_sample['img_path'] = 'tests/airplane/test.JPEG'
     evaluator = ShapeBiasMetric(
