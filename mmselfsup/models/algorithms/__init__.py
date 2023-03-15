8002504fe452e1c3527d4e99bb737d6b70eabf6c
liuyuan <3463423099@qq.com>
2023-03-15 14:59:48 +0800

    [Feature]: Add pixmim algorithm

--- mmselfsup/models/algorithms/__init__.py
+++ mmselfsup/models/algorithms/__init__.py
@@ -21,10 +21,11 @@ from .simclr import SimCLR
 from .simmim import SimMIM
 from .simsiam import SimSiam
 from .swav import SwAV
+from .pixmim import PixMIM
 
 __all__ = [
     'BaseModel', 'BarlowTwins', 'BEiT', 'BYOL', 'DeepCluster', 'DenseCL',
     'MoCo', 'NPID', 'ODC', 'RelativeLoc', 'RotationPred', 'SimCLR', 'SimSiam',
     'SwAV', 'MAE', 'MoCoV3', 'SimMIM', 'CAE', 'MaskFeat', 'MILAN', 'EVA',
-    'MixMIM'
+    'MixMIM', 'PixMIM'
 ]
