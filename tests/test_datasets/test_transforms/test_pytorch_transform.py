2ae9170bf338a875ab4deb7cb247d126d07ee157
liuyuan <3463423099@qq.com>
2023-03-22 11:51:13 +0800

    [Feature]: Add ut to mae random resized crop

---
+++ tests/test_datasets/test_transforms/test_pytorch_transform.py
@@ -0,0 +1,23 @@
+import torch
+from mmselfsup.datasets.transforms import MAERandomResizedCrop
+from PIL import Image
+import numpy as np
+
+
+def test_mae_random_resized_crop():
+    transform = dict(size=224, interpolation=3)
+
+    img_numpy = torch.rand((512, 512, 3)).numpy() * 255
+    img_numpy = img_numpy.astype(np.uint8)
+    img_pil = Image.fromarray(img_numpy)
+    results = {'img': img_pil}
+    module = MAERandomResizedCrop(**transform)
+
+    results = module(results)
+    results['img'] = np.array(results['img'])
+
+    # test transform
+    assert list(results['img'].shape) == [224, 224, 3]
+
+    # test repr
+    assert isinstance(str(module), str)
