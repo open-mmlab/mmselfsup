55e2df9d7df760205d47262847f5782d1a41fdc8
fangyixiao18 <fangyx18@hotmail.com>
2023-04-04 17:35:46 +0800

    remove repeatsampler

--- configs/selfsup/spark/classification/resnet50_8xb256-coslr-300e_in1k.py
+++ configs/selfsup/spark/classification/resnet50_8xb256-coslr-300e_in1k.py
@@ -6,63 +6,9 @@ _base_ = [
 # modification is based on ResNets RSB settings
 
 # dataset settings
-data_root = 'data/imagenet/'
-
-data_preprocessor = dict(
-    num_classes=1000,
-    # RGB format normalization parameters
-    mean=[123.675, 116.28, 103.53],
-    std=[58.395, 57.12, 57.375],
-    # convert image from BGR to RGB
-    to_rgb=True,
-)
-
-bgr_mean = data_preprocessor['mean'][::-1]
-bgr_std = data_preprocessor['std'][::-1]
-
-train_pipeline = [
-    dict(type='LoadImageFromFile'),
-    dict(
-        type='RandomResizedCrop',
-        scale=224,
-        backend='pillow',
-        interpolation='bicubic'),
-    dict(type='RandomFlip', prob=0.5, direction='horizontal'),
-    dict(
-        type='RandAugment',
-        policies='timm_increasing',
-        num_policies=2,
-        total_level=10,
-        magnitude_level=7,
-        magnitude_std=0.5,
-        hparams=dict(
-            pad_val=[round(x) for x in bgr_mean], interpolation='bicubic')),
-    dict(type='PackClsInputs'),
-]
-
-test_pipeline = [
-    dict(type='LoadImageFromFile'),
-    dict(
-        type='ResizeEdge',
-        scale=236,
-        edge='short',
-        backend='pillow',
-        interpolation='bicubic'),
-    dict(type='CenterCrop', crop_size=224),
-    dict(type='PackClsInputs')
-]
-
-train_dataloader = dict(
-    dataset=dict(data_root=data_root, pipeline=train_pipeline),
-    sampler=dict(type='RepeatAugSampler', shuffle=True),
-)
-val_dataloader = dict(
-    dataset=dict(data_root=data_root, pipeline=test_pipeline), )
-val_evaluator = dict(type='Accuracy', topk=(1, 5))
-
-# If you want standard test, please manually configure the test dataset
-test_dataloader = val_dataloader
-test_evaluator = val_evaluator
+data_root = '/nvme/dataset/classification/imagenet/'
+train_dataloader = dict(dataset=dict(data_root=data_root,))
+val_dataloader = dict(dataset=dict(data_root=data_root))
 
 # model settings
 model = dict(
@@ -78,14 +24,6 @@ model = dict(
         dict(type='CutMix', alpha=1.0)
     ]))
 
-# custom_hooks = [
-#     dict(
-#         type='EMAHook',
-#         ema_type='mmselfsup.CosineEMA',
-#         momentum=0.99,
-#         end_momentum=0.999,
-#         priority='ABOVE_NORMAL')
-# ]
 custom_hooks = [dict(type='EMAHook', momentum=1e-4, priority='ABOVE_NORMAL')]
 
 # schedule settings
